30a00395c52fad326cefc6ab700286a5429a401f
The Stig <sheparda20@gmail.com>
2021-06-04 22:44:58 +0930

    Cleanup, removed redundancy, added comments

--- lua/randomat2/events/boing.lua
+++ lua/randomat2/events/boing.lua
@@ -1,5 +1,4 @@
 local EVENT = {}
-
 EVENT.Title = "Boing"
 EVENT.Description = "Everyone can jump very high"
 EVENT.id = "boing"
@@ -7,26 +6,28 @@ EVENT.id = "boing"
 CreateConVar("randomat_boing_jump_height", 220, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Additional jump height", 0, 600)
 
 function EVENT:Begin()
-	for i, ply in pairs(self:GetAlivePlayers()) do
-		ply:SetJumpPower(ply:GetJumpPower() + GetConVar("randomat_boing_jump_height"):GetInt())
-	end
+    --Adds the set jump power to all player's jump power
+    for i, ply in pairs(player.GetAll()) do
+        ply:SetJumpPower(ply:GetJumpPower() + GetConVar("randomat_boing_jump_height"):GetInt())
+    end
 end
 
 function EVENT:End()
-	self:CleanUpHooks()
-
-	for _, ply in pairs(player.GetAll()) do
-		ply:SetJumpPower(200)
-	end
-	
+    --Sets everyone's jump power back to the default, 200
+    for _, ply in pairs(player.GetAll()) do
+        ply:SetJumpPower(200)
+    end
 end
 
 function EVENT:GetConVars()
     local sliders = {}
+
     for _, v in pairs({"jump_height"}) do
         local name = "randomat_" .. self.id .. "_" .. v
+
         if ConVarExists(name) then
             local convar = GetConVar(name)
+
             table.insert(sliders, {
                 cmd = v,
                 dsc = convar:GetHelpText(),
@@ -36,6 +37,7 @@ function EVENT:GetConVars()
             })
         end
     end
+
     return sliders
 end
 
