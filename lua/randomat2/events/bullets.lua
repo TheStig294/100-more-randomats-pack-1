1c364eb0f373887f7470ca3b4763ac28df6bcde4
The Stig <sheparda20@gmail.com>
2021-06-05 19:17:35 +0930

    Changed title, cleanup

--- lua/randomat2/events/bullets.lua
+++ lua/randomat2/events/bullets.lua
@@ -1,15 +1,12 @@
 local EVENT = {}
-
-EVENT.Title = "Bullets! My only weakness!"
+EVENT.Title = "Bullets, my only weakness!"
 EVENT.Description = "Bullet damage only"
 EVENT.id = "bullets"
 
 function EVENT:Begin()
-	self:AddHook("EntityTakeDamage", function(ent, dmginfo)
-		if IsValid(ent) and ent:IsPlayer() and dmginfo:IsBulletDamage() == false then
-			return true
-		end
-	end)
+    self:AddHook("EntityTakeDamage", function(ent, dmginfo)
+        if IsValid(ent) and ent:IsPlayer() and dmginfo:IsBulletDamage() == false then return true end
+    end)
 end
 
 Randomat:register(EVENT)
