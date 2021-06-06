5802f2643a4e6c5e8d2b6d091d7713486e76882b
The Stig <TheStig294@users.noreply.github.com>
2021-06-06 22:14:59 +0930

    Now a standalone event again

--- lua/randomat2/events/freeze2.lua
+++ lua/randomat2/events/freeze2.lua
@@ -1,4 +1,5 @@
 local EVENT = {}
+-- Fills a table with all the possible names of this randomat
 local eventnames = {}
 table.insert(eventnames, "What's this one? Oh, it's the freeze randomat...")
 table.insert(eventnames, "This is a new one! Wait, it's a freeze randomat...")
@@ -16,31 +17,79 @@ table.insert(eventnames, "We've updated our freezing policy.")
 table.insert(eventnames, "Random Freeze for everyone!")
 table.insert(eventnames, "Honey, I froze the terrorists")
 table.insert(eventnames, "Sudden Freeze!")
-table.insert(eventnames, "There are more than " .. #eventnames .. " different freeze randomat names!")
-EVENT.Title = ""
+table.insert(eventnames, "There are more than " .. #eventnames .. " different freeze randomat names")
+-- Setting a random name to this randomat
+EVENT.Title = table.Random(eventnames)
 EVENT.AltTitle = "Freeze (Randomat Puns)"
 EVENT.id = "freeze2"
-EVENT.Title = table.Random(eventnames)
+
+CreateConVar("randomat_freeze2_duration", 5, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Duration of the Freeze (in seconds)", 1, 60)
+
+CreateConVar("randomat_freeze2_timer", 30, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "How often (in seconds) the Freeze occurs", 5, 60)
 
 function EVENT:Begin()
-    Randomat:SilentTriggerEvent("freeze", self.owner)
-
+    -- Display this randomat's description after a delay
     timer.Simple(7, function()
-        self:SmallNotify("All Innocents will Freeze (and become immune) every " .. GetConVar("randomat_freeze_timer"):GetInt() .. " seconds")
+        self:SmallNotify("All Innocents will Freeze (and become immune) every " .. GetConVar("randomat_freeze2_timer"):GetInt() .. " seconds")
+    end)
+
+    local tmr = GetConVar("randomat_freeze2_timer"):GetInt()
+
+    -- Every set amount of seconds,
+    timer.Create("RdmtFreeze2Timer", tmr, 0, function()
+        -- Display a notification,
+        self:SmallNotify("Freeze!")
+
+        -- For every player still alive,
+        for _, v in ipairs(self:GetAlivePlayers()) do
+            -- If they are innocent, 
+            if Randomat:IsInnocentTeam(v, true) then
+                -- Freeze them
+                v:Freeze(true)
+                v.isFrozen = true
+
+                -- And un-freeze them after the set amount of seconds
+                timer.Simple(GetConVar("randomat_freeze_duration"):GetInt(), function()
+                    v:Freeze(false)
+                    v.isFrozen = false
+                end)
+            end
+        end
+    end)
+
+    -- Disable taking damage if a player is frozen
+    self:AddHook("EntityTakeDamage", function(ply, dmg)
+        if ply:IsValid() and ply.isFrozen then
+            dmg:ScaleDamage(0)
+        end
     end)
 end
 
-function EVENT:Condition()
-    if not ConVarExists("ttt_randomat_freeze") then return false end
-    if Randomat:CanEventRun("freeze") then return true end
+function EVENT:End()
+    -- Unfreeze any currently frozen players, and stop periodically freezing them
+    timer.Remove("RdmtFreeze2Timer")
+end
 
-    return false
+function EVENT:GetConVars()
+    local sliders = {}
+
+    for _, v in ipairs({"duration", "timer"}) do
+        local name = "randomat_" .. self.id .. "_" .. v
+
+        if ConVarExists(name) then
+            local convar = GetConVar(name)
+
+            table.insert(sliders, {
+                cmd = v,
+                dsc = convar:GetHelpText(),
+                min = convar:GetMin(),
+                max = convar:GetMax(),
+                dcm = 0
+            })
+        end
+    end
+
+    return sliders
 end
 
 Randomat:register(EVENT)
-
-hook.Add("TTTPrepareRound", "FreezeRandomatConvarCheck", function()
-    if ConVarExists("ttt_randomat_freeze") and GetConVar("ttt_randomat_freeze2"):GetBool() then
-        GetConVar("ttt_randomat_freeze"):SetBool(true)
-    end
-end)
