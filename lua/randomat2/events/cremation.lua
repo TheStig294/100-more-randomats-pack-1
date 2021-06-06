9ae92c8725ba4d6a42c0182d4d101cba5751547e
The Stig <sheparda20@gmail.com>
2021-06-06 14:25:13 +0930

    Added comments

--- lua/randomat2/events/cremation.lua
+++ lua/randomat2/events/cremation.lua
@@ -3,6 +3,7 @@ EVENT.Title = "Cremation"
 EVENT.Description = "Bodies burn after a player dies."
 EVENT.id = "cremation"
 
+-- Takes any dead player and returns their ragdoll, else returns false
 local function findcorpse(v)
     for _, ent in pairs(ents.FindByClass("prop_ragdoll")) do
         if ent.uqid == v:UniqueID() and IsValid(ent) then return ent or false end
@@ -10,18 +11,28 @@ local function findcorpse(v)
 end
 
 function EVENT:Begin()
+    -- After a player dies
     self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
+        -- After 2 seconds
         return timer.Simple(2, function()
+            -- Find their ragdoll and ignite it
             corpse = findcorpse(ply)
             corpse:Ignite(20, 10)
 
+            -- After 15 seconds,
             timer.Simple(15, function()
+                -- Find their ragdoll
                 corpse = findcorpse(ply)
 
+                -- If the round hasn't ended or the ragdoll otherwise hasn't been removed,
                 if IsValid(corpse) then
+                    -- And the ragdoll hasn't been put out (e.g. placed in water),
                     if corpse:IsOnFire() then
+                        -- Remove it
                         corpse:Remove()
                     end
+                    -- NOTE: Ragdolls on fire can't be searched, this functionality is from the base TTT gamemode
+                    -- (Used for the flare gun)
                 end
             end)
         end)
