abb66499d58e90a9ffc2368f3e625d6ad1830efc
The Stig <TheStig294@users.noreply.github.com>
2021-06-06 22:16:55 +0930

    Localised variables, cleanup, comments

--- lua/randomat2/events/future.lua
+++ lua/randomat2/events/future.lua
@@ -1,42 +1,44 @@
 local EVENT = {}
-
 EVENT.Title = "Future Proofing"
 EVENT.Description = "Buy menu items aren't given until the next round"
 EVENT.id = "future"
-
-boughtEquipment = {}
+local futureRandomatEquipment = {}
 
 function EVENT:Begin()
-	futureRandomat = true
-	for i, ply in pairs(player.GetAll()) do
-		boughtEquipment[ply] = {}
-	end
-	
-	hook.Add("TTTOrderedEquipment", "FutureRandomatEquipmentList", function(ply, equipment, is_item)
-		table.insert(boughtEquipment[ply], equipment)
-		StripEquipment(ply, equipment, is_item)
-	end)
-	
-	hook.Add("TTTBeginRound", "FutureRandomatGiveEquipment", function()
-		timer.Simple(0.1, function()
-			for i, ply in pairs(player.GetAll()) do
-				for k = 1, #boughtEquipment[ply] do
-					ClassToGive(boughtEquipment[ply][k], ply)
-				end
-			end
-		end)
-		futureRandomat = false
-	end)
-	
-	hook.Add("TTTEndRound", "FutureRandomatStopGiveEquipment", function()
-		if futureRandomat == false then
-			hook.Remove("TTTBeginRound", "FutureRandomatGiveEquipment")
-		end
-	end)
-end
+    -- Let the weapon giving hook know the randomat has activated
+    local futureRandomat = true
 
-function EVENT:End()
-	hook.Remove("TTTOrderedEquipment", "FutureRandomatEquipmentList")
+    -- Clear bought equipment table
+    for i, ply in pairs(player.GetAll()) do
+        futureRandomatEquipment[ply] = {}
+    end
+
+    -- Add bought equipment to the table and remove it from the player
+    self:AddHook("TTTOrderedEquipment", function(ply, equipment, is_item)
+        table.insert(futureRandomatEquipment[ply], equipment)
+        StripEquipment(ply, equipment, is_item)
+    end)
+
+    -- Give everyone's bought equipment at the start of the next round
+    hook.Add("TTTBeginRound", "FutureRandomatGiveEquipment", function()
+        timer.Simple(0.1, function()
+            for i, ply in pairs(player.GetAll()) do
+                for k = 1, #futureRandomatEquipment[ply] do
+                    ClassToGive(futureRandomatEquipment[ply][k], ply)
+                end
+            end
+        end)
+
+        -- Let removal hook know the weapons have been given
+        futureRandomat = false
+    end)
+
+    -- At the end of the next round, remove the weapon giving hook
+    hook.Add("TTTEndRound", "FutureRandomatStopGiveEquipment", function()
+        if futureRandomat == false then
+            hook.Remove("TTTBeginRound", "FutureRandomatGiveEquipment")
+        end
+    end)
 end
 
 Randomat:register(EVENT)
