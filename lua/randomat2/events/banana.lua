bfed2165264b606b3d2b199bfcaffa3a073060ef
The Stig <sheparda20@gmail.com>
2021-06-06 13:39:58 +0930

    Changed name, fixed convars

--- lua/randomat2/events/banana.lua
+++ lua/randomat2/events/banana.lua
@@ -1,19 +1,19 @@
 local EVENT = {}
-EVENT.Title = "Banana Peel"
-EVENT.Description = "Zero friction"
-EVENT.id = "banana"
+EVENT.Title = "Zero friction!"
+EVENT.Description = "Also, no prop damage"
+EVENT.id = "friction"
 
-local randomat_banana_friction = CreateConVar("randomat_banana_friction", "0", {FCVAR_ARCHIVE}, "Friction amount")
+CreateConVar("randomat_friction_friction", "0", {FCVAR_ARCHIVE}, "Friction amount", 0, 8)
 
-local randomat_banana_nopropdmg = CreateConVar("randomat_banana_nopropdmg", "1", {FCVAR_ARCHIVE}, "If enabled, everyone becomes immune to prop damage")
+CreateConVar("randomat_friction_nopropdmg", "1", {FCVAR_ARCHIVE}, "Immunity to prop damage, else you might die from touching props")
 
 function EVENT:Begin()
     bananaRandomat = true
-    --Setting friction to 0, by default
-    RunConsoleCommand("sv_friction", randomat_banana_friction:GetFloat())
+    -- Setting friction to 0, by default
+    RunConsoleCommand("sv_friction", GetConVar("randomat_friction_friction"):GetInt())
 
-    --Removing prop damage as props can easily unintentionally kill you while friction is set to 0, by default
-    if randomat_banana_nopropdmg:GetBool() then
+    -- Removing prop damage as props can easily unintentionally kill you while friction is set to 0, by default
+    if GetConVar("randomat_friction_nopropdmg"):GetBool() then
         self:AddHook("EntityTakeDamage", function(ent, dmginfo)
             if IsValid(ent) and ent:IsPlayer() and dmginfo:IsDamageType(DMG_CRUSH) then return true end
         end)
@@ -21,10 +21,47 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
-    --Preventing the end function running unless the begin function has run
+    -- Preventing the end function running unless this randomat has already been run
     if bananaRandomat then
         RunConsoleCommand("sv_friction", 8)
     end
+end
+
+function EVENT:GetConVars()
+    local sliders = {}
+
+    for _, v in ipairs({"friction"}) do
+        local name = "randomat_" .. self.id .. "_" .. v
+
+        if ConVarExists(name) then
+            local convar = GetConVar(name)
+
+            table.insert(sliders, {
+                cmd = v,
+                dsc = convar:GetHelpText(),
+                min = convar:GetMin(),
+                max = convar:GetMax(),
+                dcm = 0
+            })
+        end
+    end
+
+    local checks = {}
+
+    for _, v in ipairs({"nopropdmg"}) do
+        local name = "randomat_" .. self.id .. "_" .. v
+
+        if ConVarExists(name) then
+            local convar = GetConVar(name)
+
+            table.insert(checks, {
+                cmd = v,
+                dsc = convar:GetHelpText()
+            })
+        end
+    end
+
+    return sliders, checks
 end
 
 Randomat:register(EVENT)
