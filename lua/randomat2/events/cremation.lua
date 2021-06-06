832b5a1a348768ae3e316fc661331a1f92df66f2
The Stig <sheparda20@gmail.com>
2021-06-06 13:43:57 +0930

    Removed convar code for randomats that don't have them

--- lua/randomat2/events/cremation.lua
+++ lua/randomat2/events/cremation.lua
@@ -3,61 +3,6 @@ EVENT.Title = "Cremation"
 EVENT.Description = "Bodies burn after a player dies."
 EVENT.id = "cremation"
 
-function EVENT:GetConVars()
-    local sliders = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(sliders, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText(), -- The description of the ConVar
-                min = convar:GetMin(), -- The minimum value for this slider-based ConVar
-                max = convar:GetMax(), -- The maximum value for this slider-based ConVar
-                dcm = 0 -- The number of decimal points to support in this slider-based ConVar
-                
-            })
-        end
-    end
-
-    local checks = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(checks, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText() -- The description of the ConVar
-                
-            })
-        end
-    end
-
-    local textboxes = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(textboxes, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText() -- The description of the ConVar
-                
-            })
-        end
-    end
-
-    return sliders, checks, textboxes
-end
-
 local function findcorpse(v)
     for _, ent in pairs(ents.FindByClass("prop_ragdoll")) do
         if ent.uqid == v:UniqueID() and IsValid(ent) then return ent or false end
