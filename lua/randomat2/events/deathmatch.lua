d7b37ea8661d02979e26f85a9f009183b8702e49
The Stig <sheparda20@gmail.com>
2021-06-06 14:33:04 +0930

    Now strips player credits

--- lua/randomat2/events/deathmatch.lua
+++ lua/randomat2/events/deathmatch.lua
@@ -1,20 +1,27 @@
 local EVENT = {}
-
 EVENT.Title = "Team Deathmatch"
 EVENT.Description = "1/2 detectives, 1/2 traitors"
 EVENT.id = "deathmatch"
 
 function EVENT:Begin()
-	for i, ply in pairs(self:GetAlivePlayers(true)) do
-		if (i % 2) == 0 then
-			self:StripRoleWeapons(ply)
-			Randomat:SetRole(ply, ROLE_DETECTIVE)
-		else
-			self:StripRoleWeapons(ply)
-			Randomat:SetRole(ply, ROLE_TRAITOR)
-		end
-	end
-	SendFullStateUpdate()
+    -- Randomly for all alive players,
+    for i, ply in pairs(self:GetAlivePlayers(true)) do
+        if (i % 2) == 0 then
+            -- Set half of all players to detectives
+            self:StripRoleWeapons(ply)
+            Randomat:SetRole(ply, ROLE_DETECTIVE)
+            -- Remove their credits
+            ply:SetCredits(0)
+        else
+            -- Set the rest to traitors
+            self:StripRoleWeapons(ply)
+            Randomat:SetRole(ply, ROLE_TRAITOR)
+            -- Remove their credits
+            ply:SetCredits(0)
+        end
+    end
+
+    SendFullStateUpdate()
 end
 
 Randomat:register(EVENT)
