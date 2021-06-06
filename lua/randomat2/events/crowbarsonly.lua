8d415d592a69fcfba1bbef7bec6fde578dc02999
The Stig <sheparda20@gmail.com>
2021-06-06 14:24:50 +0930

    Gives a crowbar just in case, added comments

--- lua/randomat2/events/crowbarsonly.lua
+++ lua/randomat2/events/crowbarsonly.lua
@@ -1,34 +1,40 @@
 local EVENT = {}
-
 EVENT.Title = "Crowbars Only!"
 EVENT.Description = "Can only use, or be damaged by, a buffed crowbar"
 EVENT.id = "crowbarsonly"
 
 function EVENT:Begin()
-	for _, ent in pairs(ents.GetAll()) do
-		if ent.Kind == WEAPON_PISTOL or ent.Kind == WEAPON_HEAVY or ent.Kind == WEAPON_NADE and ent.AutoSpawnable then
-			ent:Remove()
-		end
-	end
-	
-	Randomat:SilentTriggerEvent("crowbar", self.owner)
-	
-	self:AddHook("EntityTakeDamage", function(ent, dmginfo)
-		if IsValid(ent) and ent:IsPlayer() and !dmginfo:IsDamageType(DMG_CLUB) then
-			return true
-		end
-	end)
-	
-	for i, ply in pairs(self:GetAlivePlayers()) do
-		timer.Simple(0.1, function()
-			if ply:GetRole() == ROLE_KILLER then
-				ply:Give("weapon_kil_crowbar")
-				ply:SelectWeapon("weapon_kil_crowbar")
-			else
-				ply:SelectWeapon("weapon_zm_improvised")
-			end
-		end)
-	end
+    -- Remove all non-buyable weapons
+    for _, ent in pairs(ents.GetAll()) do
+        if (ent.Kind == WEAPON_PISTOL or ent.Kind == WEAPON_HEAVY or ent.Kind == WEAPON_NADE) and ent.AutoSpawnable then
+            ent:Remove()
+        end
+    end
+
+    -- Buff the crowbar if that randomat can run/exists
+    if Randomat:CanEventRun("crowbar") then
+        Randomat:SilentTriggerEvent("crowbar", self.owner)
+    end
+
+    -- If someone takes damage from something that's not a crowbar, negate it
+    self:AddHook("EntityTakeDamage", function(ent, dmginfo)
+        if IsValid(ent) and ent:IsPlayer() and not dmginfo:IsDamageType(DMG_CLUB) then return true end
+    end)
+
+    -- For all alive players,
+    for i, ply in pairs(self:GetAlivePlayers()) do
+        timer.Simple(0.1, function()
+            -- If someone is a killer, give them the killer's special crowbar
+            if ply:GetRole() == ROLE_KILLER then
+                ply:Give("weapon_kil_crowbar")
+                ply:SelectWeapon("weapon_kil_crowbar")
+            else
+                -- Else, give a normal crowbar
+                ply:Give("weapon_zm_improvised")
+                ply:SelectWeapon("weapon_zm_improvised")
+            end
+        end)
+    end
 end
 
 Randomat:register(EVENT)
