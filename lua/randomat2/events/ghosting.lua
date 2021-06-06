ce596c1bc232b2024ea7a0bece065b0f7b8a36b2
The Stig <TheStig294@users.noreply.github.com>
2021-06-07 00:28:51 +0930

    Cleanup and added comments

--- lua/randomat2/events/ghosting.lua
+++ lua/randomat2/events/ghosting.lua
@@ -6,25 +6,33 @@ EVENT.id = "ghosting"
 function EVENT:Begin()
     local ghostents = {}
 
-    hook.Add("PostPlayerDeath", "HauntingRandomat", function(ply)
+    self:AddHook("PostPlayerDeath", function(ply)
+        -- After the player has finished spectating,
         timer.Simple(3, function()
+            -- Create the ghost entity using the player's model
             ghostents[ply:Nick()] = ents.Create("npc_kleiner")
             local ghostent = ghostents[ply:Nick()]
             ghostent:SetModel(ply:GetModel())
+            -- Spawn it on the player
             ghostent:SetPos(ply:GetPos())
             ghostent:Spawn()
+            -- Make it non-solid
             ghostent:SetNotSolid(true)
+            -- Make it ghostly
             ghostent:SetColor(Color(245, 245, 245, 100))
             ghostent:SetRenderMode(RENDERMODE_GLOW)
+            -- Make it invincible
             ghostent:SetNWBool("IsSpookyInv", true)
             ply:SetNWBool("IsSpooky", true)
 
+            -- Continually, for every player, 
             hook.Add("Think", "ghostRandomatUpdate", function()
                 local plys = player.GetAll()
 
                 for i = 1, #plys do
                     local ply = plys[i]
 
+                    -- That is alive, remove their ghost
                     if ply:Alive() or not ply:IsSpec() then
                         if IsValid(ghostents[ply:Nick()]) then
                             ghostent = ghostents[ply:Nick()]
@@ -34,37 +42,33 @@ function EVENT:Begin()
                         continue
                     end
 
+                    -- That doesn't have the spooky tag, ignore
                     if not ply:GetNWBool("IsSpooky") then continue end
-                    --local hasUpdated = false
-                    --if CurTime() >= tick_delay and not hasUpdated then
+                    -- Else, get their ghost entity
                     ghostent = ghostents[ply:Nick()]
 
+                    -- Check it's valid and update the ghost's position
                     if IsValid(ghostent) then
                         ghostent:SetPos(ply:GetPos())
-                        --tick_delay = CurTime() + 5
-                        --hasUpdated = true
-                        --end
                     end
                 end
             end)
         end)
     end)
 
-    local tick_delay = CurTime() + 3
-
-    hook.Add("EntityTakeDamage", "IsSpookyInvincible", function(ent, dmginfo)
+    -- Ghosts don't take damage
+    self:AddHook("EntityTakeDamage", function(ent, dmginfo)
         if ent:GetNWBool("IsSpookyInv") then return true end
     end)
 end
 
+-- Remove the ghost position update hook and remove the spooky tag from all players,
+-- which signifies the player has a ghost spawned on them
 function EVENT:End()
     hook.Remove("Think", "ghostRandomatUpdate")
-    hook.Remove("PostPlayerDeath", "HauntingRandomat")
-    hook.Remove("EntityTakeDamage", "IsSpookyInvincible")
-    local plys = player.GetAll()
 
-    for i = 1, #plys do
-        plys[i]:SetNWBool("IsSpooky", false)
+    for i, ply in pairs(player.GetAll()) do
+        ply:SetNWBool("IsSpooky", false)
     end
 end
 
--- lua/randomat2/events/goboom.lua
+++ lua/randomat2/events/goboom.lua
@@ -1,17 +1,16 @@
 local EVENT = {}
-
 EVENT.Title = "Outcome? Prop go boom."
 EVENT.Description = "Props explode when destroyed"
 EVENT.id = "goboom"
 
 function EVENT:Begin()
-	self:AddHook("PropBreak", function(ply, prop)
-		local explosion = ents.Create( "env_explosion" )
-        explosion:SetPos( prop:GetPos() )
-	    explosion:Spawn() -- Spawn the explosion
-	    explosion:SetKeyValue( "iMagnitude", "200" )
-        explosion:Fire( "Explode", 0, 0 )
-	end)
+    self:AddHook("PropBreak", function(ply, prop)
+        local explosion = ents.Create("env_explosion")
+        explosion:SetPos(prop:GetPos())
+        explosion:Spawn() -- Spawn the explosion
+        explosion:SetKeyValue("iMagnitude", "200")
+        explosion:Fire("Explode", 0, 0)
+    end)
 end
 
 Randomat:register(EVENT)
