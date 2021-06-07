84c7224c0858af81d93d0cc1e2aa8591c1da86e1
The Stig <TheStig294@users.noreply.github.com>
2021-06-07 15:36:38 +0930

    Added convar support and cleanup

--- lua/randomat2/events/livecount.lua
+++ lua/randomat2/events/livecount.lua
@@ -1,29 +1,43 @@
 AddCSLuaFile()
-
 local EVENT = {}
 
-CreateConVar("randomat_livecount_timer", 30, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Time between live counts")
+CreateConVar("randomat_livecount_timer", 30, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Time between live counts", 5, 90)
 
 EVENT.Title = "Live Count"
 EVENT.Description = "Counts no. of alive players every " .. GetConVar("randomat_livecount_timer"):GetInt() .. " seconds"
 EVENT.id = "livecount"
 
 function EVENT:Begin()
-
-	timer.Create("RandomatLiveCountTimer", GetConVar("randomat_livecount_timer"):GetInt(), 0, function()
-		local count = 0
-		
-		for i, ply in pairs(self:GetAlivePlayers(true)) do
-			count = count + 1
-		end
-		
-		self:SmallNotify("There are "..count.." people still alive")
-	end)
-
+    timer.Create("RandomatLiveCountTimer", GetConVar("randomat_livecount_timer"):GetInt(), 0, function()
+        self:SmallNotify("There are " .. #self:GetAlivePlayers() .. " people still alive")
+    end)
 end
 
 function EVENT:End()
-	timer.Remove("RandomatLiveCountTimer")
+    -- Stop triggering at the end of the round
+    timer.Remove("RandomatLiveCountTimer")
+end
+
+function EVENT:GetConVars()
+    local sliders = {}
+
+    for _, v in pairs({"timer"}) do
+        local name = "randomat_" .. self.id .. "_" .. v
+
+        if ConVarExists(name) then
+            local convar = GetConVar(name)
+
+            table.insert(sliders, {
+                cmd = v,
+                dsc = convar:GetHelpText(),
+                min = convar:GetMin(),
+                max = convar:GetMax(),
+                dcm = 0
+            })
+        end
+    end
+
+    return sliders
 end
 
 Randomat:register(EVENT)
