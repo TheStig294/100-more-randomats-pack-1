681d50c018d53eca6e2f1be2bd6536cc8525c262
The Stig <TheStig294@users.noreply.github.com>
2021-06-07 14:57:19 +0930

    Added comments

--- lua/randomat2/events/gunfire.lua
+++ lua/randomat2/events/gunfire.lua
@@ -6,17 +6,25 @@ EVENT.id = "gunfire"
 CreateConVar("randomat_gunfire_timer", 20, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Seconds before a player is ignited", 1, 120)
 
 function EVENT:Begin()
+    -- Initial player igniting timer
+    -- For all alive players,
     for i, ply in pairs(self:GetAlivePlayers()) do
+        -- After the set amount of seconds,
         timer.Create(ply:SteamID64() .. "_gunfire_timer", 1, GetConVar("randomat_gunfire_timer"):GetInt(), function()
+            -- After the timer has run out and the player is still alive,
             if timer.RepsLeft(ply:SteamID64() .. "_gunfire_timer") == 0 and ply:Alive() and not ply:IsSpec() then
+                -- Ignite them for 60 seconds and display a chat message
                 ply:Ignite(60)
                 ply:ChatPrint("Shoot to put out the fire!")
             end
         end)
     end
 
+    -- Player extinguishing hook
     self:AddHook("PlayerButtonDown", function(ply, button)
+        -- Literally just looking for the player left-clicking, alive, and having some ammo in their gun's clip when they click
         if button == MOUSE_LEFT and ply:Alive() and not ply:IsSpec() and ply:GetActiveWeapon():Clip1() ~= nil and ply:GetActiveWeapon():Clip1() > 0 then
+            -- Restarting player igniting timer
             timer.Create(ply:SteamID64() .. "_gunfire_timer", 1, GetConVar("randomat_gunfire_timer"):GetInt(), function()
                 if timer.RepsLeft(ply:SteamID64() .. "_gunfire_timer") == 0 and ply:Alive() and not ply:IsSpec() then
                     ply:Ignite(60)
@@ -24,10 +32,13 @@ function EVENT:Begin()
                 end
             end)
 
+            -- And putting out the fire
             ply:Extinguish()
         end
     end)
 
+    -- Original player extinguishing hook, apparently doesn't work for projectile-based guns,
+    -- which is why the hook above was added
     self:AddHook("EntityFireBullets", function(ply, data)
         if ply:IsPlayer() then
             timer.Create(ply:SteamID64() .. "_gunfire_timer", 1, GetConVar("randomat_gunfire_timer"):GetInt(), function()
@@ -43,6 +54,7 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
+    -- Put all players out
     for i, ply in pairs(self:GetPlayers()) do
         timer.Remove(ply:SteamID64() .. "_gunfire_timer")
         ply:Extinguish()
--- lua/randomat2/events/hud.lua
+++ lua/randomat2/events/hud.lua
@@ -7,9 +7,11 @@ util.AddNetworkString("HUDRandomatHint")
 util.AddNetworkString("HUDRandomatEnd")
 
 function EVENT:Begin()
+    -- Hides the start-of-round role hint
     net.Start("HUDRandomatHint")
     net.Broadcast()
 
+    -- Hides the rest of the HUD, including the scoreboard
     timer.Simple(5, function()
         net.Start("HUDRandomat")
         net.Broadcast()
@@ -17,6 +19,7 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
+    -- Removes the client-side HUD-hiding hooks for all players
     net.Start("HUDRandomatEnd")
     net.Broadcast()
 end
