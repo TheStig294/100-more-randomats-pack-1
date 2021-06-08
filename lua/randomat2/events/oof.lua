604e69154ed77e854f3d12816d87182e2b9a320d
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 01:11:14 +0930

    Cleanup and added comments

--- lua/randomat2/events/oof.lua
+++ lua/randomat2/events/oof.lua
@@ -1,18 +1,16 @@
 local EVENT = {}
-
 EVENT.Title = "Oof"
 EVENT.Description = "Replaces death sound with Roblox oof"
 EVENT.id = "oof"
 
 function EVENT:Begin()
-	hook.Add("DoPlayerDeath", "OofDeathSound", function(ply, attacker, dmginfo)
-		dmginfo:SetDamageType(DMG_SLASH)
-		sound.Play("oof/oof.wav", ply:GetShootPos(), 90, 100, 1)
-	end)
-end
-
-function EVENT:End()
-	hook.Remove("DoPlayerDeath", "OofDeathSound")
+    -- Whenever a player dies
+    self:AddHook("DoPlayerDeath", function(ply, attacker, dmginfo)
+        -- Silence their usual death noise
+        dmginfo:SetDamageType(DMG_SLASH)
+        -- And play the Roblox oof instead
+        sound.Play("oof/oof.wav", ply:GetShootPos(), 90, 100, 1)
+    end)
 end
 
 Randomat:register(EVENT)
