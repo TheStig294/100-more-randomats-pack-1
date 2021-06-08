cfbe641c71bde4e8f6f3c287bf1f4b59ef4833a9
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 02:38:20 +0930

    Cleanup and added comments

--- lua/randomat2/events/pockets.lua
+++ lua/randomat2/events/pockets.lua
@@ -1,27 +1,31 @@
 local EVENT = {}
-
 EVENT.Title = "What did WE find in our pockets?"
 EVENT.Description = "Gives everyone the same random buyable weapon"
 EVENT.id = "pockets"
 
 function EVENT:Begin()
+    -- Get the table of all weapons installed
     local weps = weapons.GetList()
     local give_weps = {}
+
+    -- Filter out the non-buyable weapons
     for i = 1, #weps do
-      local wep = weps[i]
-      if wep and (wep.CanBuy and #wep.CanBuy > 0) then
-        give_weps[#give_weps + 1] = wep
-      end
+        local wep = weps[i]
+
+        if wep and (wep.CanBuy and #wep.CanBuy > 0) then
+            give_weps[#give_weps + 1] = wep
+        end
     end
+
+    -- Choose a random weapon and get all alive players
     local item = give_weps[math.random(#give_weps)]
-    local print_name = item.PrintName or WEPS.GetClass(item)
+    local plys = self:GetAlivePlayers()
 
-
-	local plys = util.GetAlivePlayers()
-	for i = 1, #plys do
-		local ply = plys[i]
-		ply:Give(WEPS.GetClass(item))
-	end
+    -- Give everyone that random weapon
+    for i = 1, #plys do
+        local ply = plys[i]
+        ply:Give(WEPS.GetClass(item))
+    end
 end
 
 Randomat:register(EVENT)
--- lua/randomat2/events/potato.lua
+++ lua/randomat2/events/potato.lua
@@ -4,7 +4,9 @@ EVENT.Description = "Someone has the hot potato, pass it on or else you explode!
 EVENT.id = "potato"
 
 hook.Add("PreRegisterSWEP", "HotPotatoGetRandomatFunctions", function(SWEP, class)
+    -- If it exists,
     if class == "weapon_ttt_hotpotato" then
+        --  Grab the hot potato's activate and clean-up functions,
         function RandomatActivatePotato(ply, PotatoChef)
             SWEP:PotatoTime(ply, PotatoChef)
         end
@@ -13,21 +15,26 @@ hook.Add("PreRegisterSWEP", "HotPotatoGetRandomatFunctions", function(SWEP, clas
             fn_CleanUpAll()
         end
 
+        -- And set a global bool to say it was found
         SetGlobalBool("PotatoRandomatExists", true)
     end
 end)
 
 function EVENT:Begin()
+    -- Choose a random player
     local ply = table.Random(self:GetAlivePlayers())
 
+    -- Ensure the hot potato hooks are cleaned up
     hook.Add("TTTPrepareRound", ply:Name() .. "_RoundRestartCleanup", function()
         RandomatPotatoCleanup()
     end)
 
+    -- And activate the hot potato on the chosen player, setting the attacker to be themselves
     RandomatActivatePotato(ply, ply)
 end
 
 function EVENT:Condition()
+    -- Only activate if the hot potato weapon was found
     return GetGlobalBool("PotatoRandomatExists", false)
 end
 
