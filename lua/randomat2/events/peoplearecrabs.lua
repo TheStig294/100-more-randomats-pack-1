723c7315b55ddb9f0226786339579bde54408fe7
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 01:13:36 +0930

    Added comments, cleanup, and prevented multi-jump

--- lua/randomat2/events/peoplearecrabs.lua
+++ lua/randomat2/events/peoplearecrabs.lua
@@ -2,17 +2,28 @@ local EVENT = {}
 EVENT.Title = "People are Crabs"
 EVENT.Description = "You can only walk sideways"
 EVENT.id = "peoplearecrabs"
+local orginalJumps = 1
 
 function EVENT:Begin()
-    hook.Add("SetupMove", "PeopleAreCrabs", function(ply, mv, cmd)
+    -- Prevent people from walking forwards or backwards
+    self:AddHook("SetupMove", function(ply, mv, cmd)
         if ply:IsTerror() then
             mv:SetForwardSpeed(0)
         end
     end)
+
+    -- Prevent people from multi-jumping if a mod that adds it is installed
+    if ConVarExists("multijump_default_jumps") then
+        orginalJumps = GetConVar("multijump_default_jumps"):GetInt()
+        GetConVar("multijump_default_jumps"):SetInt(0)
+    end
 end
 
 function EVENT:End()
-    hook.Remove("SetupMove", "PeopleAreCrabs")
+    -- Re-enable multi-jumping
+    if ConVarExists("multijump_default_jumps") then
+        GetConVar("multijump_default_jumps"):SetInt(orginalJumps)
+    end
 end
 
 Randomat:register(EVENT)
