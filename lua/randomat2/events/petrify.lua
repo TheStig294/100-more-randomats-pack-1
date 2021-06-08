6a942bb4f84a33bc7ef436fe663442b52cdfcc97
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 01:28:30 +0930

    Cleanup

--- lua/randomat2/events/petrify.lua
+++ lua/randomat2/events/petrify.lua
@@ -3,61 +3,6 @@ EVENT.Title = "Petrify!"
 EVENT.Description = "Turns players into a stone like figure, playing a quite annoying sound when they move."
 EVENT.id = "petrify"
 
-function EVENT:GetConVars()
-    local sliders = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(sliders, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText(), -- The description of the ConVar
-                min = convar:GetMin(), -- The minimum value for this slider-based ConVar
-                max = convar:GetMax(), -- The maximum value for this slider-based ConVar
-                dcm = 0 -- The number of decimal points to support in this slider-based ConVar
-                
-            })
-        end
-    end
-
-    local checks = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(checks, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText() -- The description of the ConVar
-                
-            })
-        end
-    end
-
-    local textboxes = {}
-
-    for _, v in pairs({}) do
-        local name = "randomat_" .. self.id .. "_" .. v
-
-        if ConVarExists(name) then
-            local convar = GetConVar(name)
-
-            table.insert(textboxes, {
-                cmd = v, -- The command extension (e.g. everything after "randomat_example_")
-                dsc = convar:GetHelpText() -- The description of the ConVar
-                
-            })
-        end
-    end
-
-    return sliders, checks, textboxes
-end
-
 -- Turn player to stone
 local function petrify(ply)
     if ply then
@@ -68,7 +13,7 @@ local function petrify(ply)
 end
 
 function EVENT:Begin()
-    hook.Add("PlayerSpawn", "randomatDefaultPlayer", function(ply)
+    self:AddHook("PlayerSpawn", function(ply)
         -- Petrify on spawn
         if ply then
             timer.Simple(0.5, petrify, ply)
@@ -82,7 +27,7 @@ function EVENT:Begin()
     end
 
     -- Player sound when moving
-    hook.Add("Move", "randomatDefaultPlayerMove", function(ply, mv)
+    self:AddHook("Move", function(ply, mv)
         if ply and ply:Alive() then
             if mv:GetVelocity():Length() > 10 and not ply.soundPlaying and ply:IsOnGround() then
                 ply:EmitSound("physics\\concrete\\concrete_scrape_smooth_loop1.wav", 30)
@@ -95,7 +40,7 @@ function EVENT:Begin()
     end)
 
     -- Stop sound on death
-    hook.Add("DoPlayerDeath", "randomatDefaultPlayerDeath", function(ply)
+    self:AddHook("DoPlayerDeath", function(ply)
         if ply.soundPlaying then
             ply:StopSound("physics\\concrete\\concrete_scrape_smooth_loop1.wav")
         end
@@ -103,11 +48,6 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
-    -- Clean up hooks
-    hook.Remove("PlayerSpawn", "randomatDefaultPlayer")
-    hook.Remove("Move", "randomatDefaultPlayerMove")
-    hook.Remove("DoPlayerDeath", "randomatDefaultPlayerDeath")
-
     -- Stop Sound
     for i, ply in ipairs(self:GetPlayers()) do
         ply:StopSound("physics\\concrete\\concrete_scrape_smooth_loop1.wav")
