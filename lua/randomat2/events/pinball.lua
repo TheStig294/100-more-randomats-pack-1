706895fa4b0ae6df40fc04e0b8e7194abd894040
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 01:42:08 +0930

    Cleanup and added comments

--- lua/randomat2/events/pinball.lua
+++ lua/randomat2/events/pinball.lua
@@ -5,6 +5,26 @@ EVENT.Title = "Pinball People"
 EVENT.Description = "When two players collide, they will be sent flying in opposite directions."
 EVENT.id = "pinball"
 
+function EVENT:Begin()
+    -- Allow us to use the following hook on all players
+    for i, ply in pairs(self:GetPlayers()) do
+        ply:SetCustomCollisionCheck(true)
+    end
+
+    -- When two entities collide
+    self:AddHook("ShouldCollide", function(ply, ply2)
+        -- And they are both players,
+        if (ply and ply2) and (ply:IsPlayer() and ply2:IsPlayer()) then
+            -- And they are close to each other
+            if ply:GetPos():DistToSqr(ply2:GetPos()) < 1300 then
+                -- Send them flying away from each other
+                velVector = Vector(ply:GetPos().x - ply2:GetPos().x, ply:GetPos().y - ply2:GetPos().y, ply:GetPos().z - ply2:GetPos().z)
+                ply:SetVelocity(velVector * GetConVar("randomat_pinball_mul"):GetInt() * 10)
+            end
+        end
+    end)
+end
+
 function EVENT:GetConVars()
     local sliders = {}
 
@@ -58,26 +78,6 @@ function EVENT:GetConVars()
     end
 
     return sliders, checks, textboxes
-end
-
-function EVENT:Begin()
-    -- Allow us to see collision
-    for i, ply in pairs(self:GetPlayers()) do
-        ply:SetCustomCollisionCheck(true)
-    end
-
-    hook.Add("ShouldCollide", "RandomatPinball", function(ply, ply2)
-        if (ply and ply2) and (ply:IsPlayer() and ply2:IsPlayer()) then
-            if ply:GetPos():DistToSqr(ply2:GetPos()) < 1300 then
-                velVector = Vector(ply:GetPos().x - ply2:GetPos().x, ply:GetPos().y - ply2:GetPos().y, ply:GetPos().z - ply2:GetPos().z)
-                ply:SetVelocity(velVector * GetConVar("randomat_pinball_mul"):GetInt() * 10)
-            end
-        end
-    end)
-end
-
-function EVENT:End()
-    hook.Remove("ShouldCollide", "RandomatPinball")
 end
 
 Randomat:register(EVENT)
