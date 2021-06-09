a37acd271c3434b8c29b895da5a4dd32aee29c54
The Stig <TheStig294@users.noreply.github.com>
2021-06-10 01:01:02 +0930

    Removed redundant condition, added comments

--- lua/randomat2/events/yogsmurder.lua
+++ lua/randomat2/events/yogsmurder.lua
@@ -8,8 +8,10 @@ util.AddNetworkString("YogsMurderRandomatEnd")
 EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
 
 function EVENT:Begin()
+    -- Trigger the murder randomat
     Randomat:SilentTriggerEvent("murder", self.owner)
 
+    -- After 1 second, start the special popup and sound for all players
     timer.Simple(1, function()
         net.Start("YogsMurderRandomat")
         net.Broadcast()
@@ -19,18 +21,20 @@ function EVENT:Begin()
         end
     end)
 
+    -- After 5 seconds, remove the popup
     timer.Simple(5, function()
         net.Start("YogsMurderRandomatEnd")
         net.Broadcast()
 
+        -- And give a hint to all players on what to do during the murder randomat
         for i, ply in pairs(self:GetPlayers()) do
             ply:ChatPrint("Gather guns for a revolver! \nAll traitors have knives...")
         end
     end)
 end
 
+-- Do not trigger if the murder randomat doesn't exist or isn't turned on
 function EVENT:Condition()
-    if not ConVarExists("ttt_randomat_murder") then return false end
     if Randomat:CanEventRun("murder") then return true end
 
     return false
@@ -38,6 +42,7 @@ end
 
 Randomat:register(EVENT)
 
+-- If this randomat is on, but the murder randomat isn't, turn the murder randomat on
 hook.Add("TTTPrepareRound", "YogsMurderRandomatConvarCheck", function()
     if ConVarExists("ttt_randomat_murder") and GetConVar("ttt_randomat_yogsmurder"):GetBool() then
         GetConVar("ttt_randomat_murder"):SetBool(true)
