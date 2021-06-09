a85c7004010b79e63da5f8e4c919eaba2db50b7b
The Stig <TheStig294@users.noreply.github.com>
2021-06-10 01:00:36 +0930

    Added comments and cleanup

--- lua/randomat2/events/unconventional.lua
+++ lua/randomat2/events/unconventional.lua
@@ -1,16 +1,18 @@
 local EVENT = {}
-
 EVENT.Title = "Unconventional Healing"
 EVENT.Description = "Fire, explosion and fall damage heals!"
 EVENT.id = "unconventional"
 
 function EVENT:Begin()
-	self:AddHook("EntityTakeDamage", function(ent, dmginfo)
-		if IsValid(ent) and ent:IsPlayer() and (dmginfo:IsDamageType(DMG_BURN) or dmginfo:IsDamageType(DMG_FALL) or dmginfo:IsDamageType(DMG_BLAST)) then
-			ent:SetHealth(math.min(ent:Health() + dmginfo:GetDamage(),ent:GetMaxHealth()))
-			return true
-		end
-	end)
+    self:AddHook("EntityTakeDamage", function(ent, dmginfo)
+        if IsValid(ent) and ent:IsPlayer() and (dmginfo:IsDamageType(DMG_BURN) or dmginfo:IsDamageType(DMG_FALL) or dmginfo:IsDamageType(DMG_BLAST)) then
+            -- Heal a player by the damage they would take, don't set them above max health
+            ent:SetHealth(math.min(ent:Health() + dmginfo:GetDamage(), ent:GetMaxHealth()))
+            -- Negate the damage they would take
+
+            return true
+        end
+    end)
 end
 
 Randomat:register(EVENT)
--- lua/randomat2/events/whoswho.lua
+++ lua/randomat2/events/whoswho.lua
@@ -2,73 +2,6 @@ local EVENT = {}
 EVENT.Title = "Who's Who?"
 EVENT.Description = "Everyone swaps playermodels"
 EVENT.id = "whoswho"
----- Define Randomat ULX Table Entries --------------------------------------------
--- This follows the randomatULX table formula
--- If you cant figure out how it should be layed out you probably shouldnt be here...
-local events = {}
-events["whoswho"] = {}
-events["whoswho"].name = "whoswho"
-events["whoswho"].sdr = {}
-slider = events["whoswho"].sdr
-slider[1] = {}
-slider[1].cmd = "min"
-slider[1].dsc = "Min time before whoswho runs (def. 15)"
-slider[1].min = "1"
-slider[1].max = "120"
-slider[2] = {}
-slider[2].cmd = "max"
-slider[2].dsc = "Max time before whoswho runs (def. 30)"
-slider[2].min = "10"
-slider[2].max = "240"
-events["whoswho"].chk = {}
-check = events["whoswho"].chk
-check[1] = {}
-check[1].cmd = "repetition"
-check[1].dsc = "Should we stop the playermodel repeating (def 1))"
-events["whoswho"].chk = {}
-check = events["whoswho"].chk
-check[2] = {}
-check[2].cmd = "disguised"
-check[2].dsc = "Should we disguise the player to hide names (def. 1)"
-
----- Commands to add to Randomat ULX command table --------------------------------
---These must be created for all convars that
---the above sliders or checkboxs want to change
--- New section to allow for transfer of commands easily
-events["whoswho"].commands = {"ttt_randomat_whoswho", "randomat_whoswho_min", "randomat_whoswho_max", "randomat_whoswho_repetition", "randomat_whoswho_disguised",}
-
-local eventJSON = util.TableToJSON(events) -- Converts Table to string for sending
----- Check for Data File and our event --------------------------------------------
-eventJSON = eventJSON .. "\n" -- Add the line end or shit breaks
-local data = file.Read("randomataddons.txt", "DATA") -- Check for file, if it doesnt exist create it.
-
--- We do this here to check if we are the first addon
-if not data then
-    file.Write("randomataddons.txt", eventJSON) -- This also allows for easy deleting/refreshing of file
-    print("[RANDOMAT] [WHOS WHO] No randomataddons file found, creating one")
-end
-
-data = file.Read("randomataddons.txt", "DATA") -- File still doesnt exist, must be an issue...
-if not data then return end
-print("[RANDOMAT] [WHOS WHO] Found randomataddons.txt")
-local alreadyExists = false
-local tabdata = string.Split(data, "\n") -- Store Table in JSON format to file for reading by randomatULX
-
--- However we need to check if our addon already exists in file
-for _, line in ipairs(tabdata) do
-    if line .. "\n" == eventJSON then
-        print("[RANDOMAT] [WHOS WHO] Event found in randomataddons list")
-        alreadyExists = true
-    end
-end
-
--- JSON Table not found in file, writing to file
-if (not alreadyExists) then
-    print("[RANDOMAT] [WHOS WHO] Did not find event in list, adding") -- PLS NOTE, if the table is altered it will add the entry again
-    file.Append("randomataddons.txt", eventJSON)
-end
-
-----------------------------------------------------------------------
 local swapModels = {}
 local playerModels = {}
 local remainingModels = {}
@@ -93,16 +26,22 @@ function EVENT:Begin()
     end
 
     timer.Simple(2, function()
+        -- Initially add all player's models to the pool of models not yet picked
         table.Add(remainingModels, playerModels)
 
+        -- For all alive players,
         for k, v in pairs(self:GetPlayers(true)) do
+            -- Randomly choose a playermodel that hasn't yet been chosen
             local chosenModel = table.Random(remainingModels)
             swapModels[v] = chosenModel
+            -- Set them to that model
             v:SetModel(chosenModel)
+            -- And remove that model from the pool of possible playermodels
             table.RemoveByValue(remainingModels, chosenModel)
         end
     end)
 
+    -- Whenever a player is about to set their model (e.g. being revived), set them to their swapped playermodel
     self:AddHook("PlayerSetModel", function(ply)
         timer.Simple(0.1, function()
             ply:SetModel(swapModels[ply])
@@ -111,6 +50,7 @@ function EVENT:Begin()
 end
 
 function EVENT:End()
+    -- Clear the used tables for next time the randomat is triggered
     table.Empty(swapModels)
     table.Empty(remainingModels)
 
