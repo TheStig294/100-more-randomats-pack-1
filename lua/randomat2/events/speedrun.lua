d20c9c694a55b870efdd0208c965fe07a6f9d6c6
The Stig <TheStig294@users.noreply.github.com>
2021-06-10 00:08:46 +0930

    Cleanup, added comment

--- lua/randomat2/events/speedrun.lua
+++ lua/randomat2/events/speedrun.lua
@@ -3,19 +3,23 @@ local EVENT = {}
 CreateConVar("randomat_speedrun_time", 60, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Time in seconds the round will last", 5)
 
 EVENT.Title = "Speedrun!"
-EVENT.Description = GetConVar("randomat_speedrun_time"):GetInt().." second round"
+EVENT.Description = GetConVar("randomat_speedrun_time"):GetInt() .. " second round"
 EVENT.id = "speedrun"
 
 function EVENT:Begin()
-	SetGlobalFloat("ttt_round_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
+    -- This global float controls the time the round will end, part of base TTT
+    SetGlobalFloat("ttt_round_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
 end
 
 function EVENT:GetConVars()
     local sliders = {}
+
     for _, v in pairs({"time"}) do
         local name = "randomat_" .. self.id .. "_" .. v
+
         if ConVarExists(name) then
             local convar = GetConVar(name)
+
             table.insert(sliders, {
                 cmd = v,
                 dsc = convar:GetHelpText(),
@@ -25,6 +29,7 @@ function EVENT:GetConVars()
             })
         end
     end
+
     return sliders
 end
 
