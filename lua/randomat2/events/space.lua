aedae05ea74e1a49a63be1975576d9be78a73830
The Stig <TheStig294@users.noreply.github.com>
2021-06-09 23:44:34 +0930

    Added comments

--- lua/randomat2/events/space.lua
+++ lua/randomat2/events/space.lua
@@ -4,13 +4,17 @@ EVENT.Description = "When you hold space, you slow down"
 EVENT.id = "space"
 
 function EVENT:Begin()
+    -- When a player holds the spacebar,
     self:AddHook("PlayerButtonDown", function(ply, button)
         if button == KEY_SPACE then
+            -- 1/2 the speed of all their movement
             ply:SetLaggedMovementValue(0.5)
+            -- And set a flag to make them immune to fall damage
             ply:SetNWBool("SpaceFallImmune", true)
         end
     end)
 
+    -- Undo this when they release the spacebar
     self:AddHook("PlayerButtonUp", function(ply, button)
         if button == KEY_SPACE then
             ply:SetLaggedMovementValue(1)
@@ -18,12 +22,14 @@ function EVENT:Begin()
         end
     end)
 
+    -- Make spacebar-holding players immune to fall damage
     self:AddHook("EntityTakeDamage", function(ent, dmginfo)
         if IsValid(ent) and ent:IsPlayer() and dmginfo:IsDamageType(DMG_FALL) and ent:GetNWBool("SpaceFallImmune") then return true end
     end)
 end
 
 function EVENT:End()
+    -- Set all players to normal speed and remove their fall damage flag
     for i, ply in pairs(self:GetPlayers()) do
         ply:SetLaggedMovementValue(1)
         ply:SetNWBool("SpaceFallImmune", false)
