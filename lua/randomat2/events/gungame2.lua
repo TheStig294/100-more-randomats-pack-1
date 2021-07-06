022157d109bdc8884b598de5ee2889b1c37adefc
The Stig <TheStig294@users.noreply.github.com>
2021-07-06 16:03:11 +0930

    Attempt to fix not working on dedicated servers

--- lua/randomat2/events/gungame2.lua
+++ lua/randomat2/events/gungame2.lua
@@ -4,40 +4,6 @@ EVENT.Description = "Change weapons when someone dies"
 EVENT.id = "gungame2"
 -- Declares this randomat a 'Weapon Override' randomat, meaning it cannot trigger if another Weapon Override randomat has triggered in the round
 EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
-local weps = {}
-
--- Function that swaps everyone to a new weapon
-local function GiveWeps()
-    timer.Simple(0.1, function()
-        -- For all players,
-        for _, v in ipairs(player.GetAll()) do
-            -- See if they are currently holding a weapon that will be swapped
-            local ac = false
-
-            if v:GetActiveWeapon().Kind == WEAPON_HEAVY or v:GetActiveWeapon().Kind == WEAPON_PISTOL then
-                ac = true
-            end
-
-            -- Remove their pistol and main gun
-            for _, wep in ipairs(v:GetWeapons()) do
-                if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
-                    v:StripWeapon(wep.ClassName)
-                end
-            end
-
-            -- Give them a random weapon
-            local wepGiven = table.Random(weps)
-            v:Give(wepGiven.ClassName)
-            -- Reset FOV to unscope
-            v:SetFOV(0, 0.2)
-
-            -- If they were holding a removed weapon, force them to select the new one
-            if ac then
-                v:SelectWeapon(wepGiven.ClassName)
-            end
-        end
-    end)
-end
 
 function EVENT:Begin()
     -- Remove all weapons from the ground, except grenades
@@ -48,7 +14,7 @@ function EVENT:Begin()
     end
 
     -- Add all floor weapons to a table to choose from
-    weps = {}
+    local weps = {}
 
     for _, wep in ipairs(weapons.GetList()) do
         if wep.AutoSpawnable and (wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL) then
@@ -57,11 +23,63 @@ function EVENT:Begin()
     end
 
     -- Give everyone their first weapon
-    GiveWeps()
+    for _, v in ipairs(player.GetAll()) do
+        -- See if they are currently holding a weapon that will be swapped
+        local ac = false
+
+        if v:GetActiveWeapon().Kind == WEAPON_HEAVY or v:GetActiveWeapon().Kind == WEAPON_PISTOL then
+            ac = true
+        end
+
+        -- Remove their pistol and main gun
+        for _, wep in ipairs(v:GetWeapons()) do
+            if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
+                v:StripWeapon(wep.ClassName)
+            end
+        end
+
+        -- Give them a random weapon
+        local wepGiven = table.Random(weps)
+        v:Give(wepGiven.ClassName)
+        -- Reset FOV to unscope
+        v:SetFOV(0, 0.2)
+
+        -- If they were holding a removed weapon, force them to select the new one
+        if ac then
+            v:SelectWeapon(wepGiven.ClassName)
+        end
+    end
 
     -- Give everyone a new weapon when someone dies
     self:AddHook("PlayerDeath", function(victim, inflictor, attacker)
-        GiveWeps()
+        timer.Simple(0.1, function()
+            for _, v in ipairs(player.GetAll()) do
+                -- See if they are currently holding a weapon that will be swapped
+                local ac = false
+
+                if v:GetActiveWeapon().Kind == WEAPON_HEAVY or v:GetActiveWeapon().Kind == WEAPON_PISTOL then
+                    ac = true
+                end
+
+                -- Remove their pistol and main gun
+                for _, wep in ipairs(v:GetWeapons()) do
+                    if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
+                        v:StripWeapon(wep.ClassName)
+                    end
+                end
+
+                -- Give them a random weapon
+                local wepGiven = table.Random(weps)
+                v:Give(wepGiven.ClassName)
+                -- Reset FOV to unscope
+                v:SetFOV(0, 0.2)
+
+                -- If they were holding a removed weapon, force them to select the new one
+                if ac then
+                    v:SelectWeapon(wepGiven.ClassName)
+                end
+            end
+        end)
     end)
 end
 
