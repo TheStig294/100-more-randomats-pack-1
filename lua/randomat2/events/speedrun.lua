8abc783d0752044ffd35630a1a4a8344fcc89a8a
The Stig <TheStig294@users.noreply.github.com>
2021-10-24 15:12:01 +1030

    Fixed round time not changing if haste mode was on

--- lua/randomat2/events/speedrun.lua
+++ lua/randomat2/events/speedrun.lua
@@ -5,10 +5,37 @@ CreateConVar("randomat_speedrun_time", 60, {FCVAR_NOTIFY, FCVAR_ARCHIVE}, "Time 
 EVENT.Title = "Speedrun!"
 EVENT.Description = GetConVar("randomat_speedrun_time"):GetInt() .. " second round"
 EVENT.id = "speedrun"
+local speedrunRandomat = false
+local hasteMode = false
+local hasteMinutes = 0.5
 
 function EVENT:Begin()
+    speedrunRandomat = true
+
     -- This global float controls the time the round will end, part of base TTT
-    SetGlobalFloat("ttt_round_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
+    if hasteMode then
+        GetConVar("ttt_haste"):SetBool(false)
+        GetConVar("ttt_haste_minutes_per_death"):SetFloat(0)
+        SetGlobalFloat("ttt_haste_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
+        SetGlobalFloat("ttt_round_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
+    else
+        SetGlobalFloat("ttt_round_end", CurTime() + GetConVar("randomat_speedrun_time"):GetInt())
+    end
+end
+
+function EVENT:End()
+    -- If the begin function has run, set the zombie prime weapons setting to what it was,
+    if speedrunRandomat then
+        GetConVar("ttt_zombie_prime_only_weapons"):SetBool(initialPrimeOnlyWeapons)
+
+        if hasteMode then
+            GetConVar("ttt_haste"):SetBool(true)
+            GetConVar("ttt_haste_minutes_per_death"):SetFloat(hasteMinutes)
+        end
+
+        -- And prevent the end function from being run until this randomat triggers again
+        speedrunRandomat = false
+    end
 end
 
 function EVENT:GetConVars()
