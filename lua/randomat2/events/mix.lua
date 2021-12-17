7b4c924881d1bb2666ff2413123613486bba2e2d
The Stig <TheStig294@users.noreply.github.com>
2021-12-17 13:48:02 +1030

    Weapon slots removed for this event, now tries to give you a weapon you don't already have

--- lua/randomat2/events/mix.lua
+++ lua/randomat2/events/mix.lua
@@ -4,29 +4,46 @@ EVENT.Description = "When you buy something, you get a random weapon instead"
 EVENT.id = "mix"
 
 function EVENT:Begin()
+    -- TTT by default uses 8 different slots for weapons, if you're using TTT2 or something that adds more weapon slots where slots 100 and above become meaningful, this might cause unexpected behaviour... (But the randomat event itself should still work)
+    local wepSlot = 100
+
     -- Whenever someone buys something, 
     self:AddHook("TTTOrderedEquipment", function(ply, equipment, is_item)
         -- Remove what they bought
         StripEquipment(ply, equipment, is_item)
 
         -- Wait a split second
-        timer.Simple(0.2, function()
+        timer.Simple(0, function()
             -- Get all weapons
             local weps = weapons.GetList()
+            local playerWeps = ply:GetWeapons()
             local give_weps = {}
 
             -- Filter out the un-buyable weapons
             for i = 1, #weps do
                 local wep = weps[i]
+                local weaponOwned = false
 
-                if wep and (wep.CanBuy and #wep.CanBuy > 0) then
+                -- Filter out all the weapons the player already owns
+                for j, wepPly in ipairs(playerWeps) do
+                    if wepPly == wep then
+                        weaponOwned = true
+                    end
+                end
+
+                if wep and (wep.CanBuy and #wep.CanBuy > 0) and not weaponOwned then
                     give_weps[#give_weps + 1] = wep
                 end
             end
 
             -- Select a random weapon and give it to the player
-            local item = give_weps[math.random(#give_weps)]
-            ply:Give(WEPS.GetClass(item))
+            if not table.IsEmpty(give_weps) then
+                local item = give_weps[math.random(#give_weps)]
+                -- Override the weapon's slot so one is always given
+                item.Kind = wepSlot
+                ply:Give(WEPS.GetClass(item))
+                wepSlot = wepSlot + 1
+            end
         end)
     end)
 end
