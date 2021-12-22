247e6b21e935a59626f11afdbdaa2148de18efae
The Stig <TheStig294@users.noreply.github.com>
2021-12-22 14:53:55 +1030

    Sound no longer randomised to fix projectile-based guns not changing sound. May fix this properly in the future

--- lua/randomat2/cl_events/cl_sahmin.lua
+++ lua/randomat2/cl_events/cl_sahmin.lua
@@ -1,52 +1,38 @@
---Replaces all gun sounds with silence (plays a silent sound file)
---In the server-side of this randomat, a hook adds the 'sahmin' sound
---This new method of replacing the gun sound allows it to play for every bullet shot
---Rather than just the first bullet shot after the mouse is held down
-local sahmin_sound = "weapons/silence.mp3"
+-- local sahmin_sounds = {Sound("weapons/sahmin1.mp3"), Sound("weapons/sahmin2.mp3"), Sound("weapons/sahmin3.mp3"), Sound("weapons/sahmin4.mp3"), Sound("weapons/sahmin5.mp3")}
+-- local sahmin_index = 1
+local sahmin_sound = Sound("weapons/sahmin1.mp3")
 local hooked = false
 
-local function FixWeapon(wep)
-    if not IsValid(wep) or not wep.Primary then return end
-    wep.Primary.OriginalSound = wep.Primary.Sound
-    wep.Primary.Sound = sahmin_sound
-end
-
 net.Receive("TriggerSahmin", function()
-    for _, wep in pairs(LocalPlayer():GetWeapons()) do
-        FixWeapon(wep)
+    -- timer.Create("RandomatSahminSoundClient", 1, 0, function()
+    --     sahmin_index = util.SharedRandom("RandomatSahminSound", 1, #sahmin_sounds, CurTime())
+    -- end)
+    -- This event can be called multiple times but we only want to add the hook once
+    if not hooked then
+        hook.Add("EntityEmitSound", "SahminOverrideHook", function(data) return Randomat:OverrideWeaponSoundData(data, sahmin_sound) end)
+        hooked = true
     end
 
-    -- This even can be called multiple times but we only want to add the hook once
-    -- [Doesn't actually matter now since we're only adding silence, adding more silence won't make anything louder ;)]
-    if hooked then return end
+    local client = LocalPlayer()
+    if not IsValid(client) then return end
 
-    hook.Add("EntityEmitSound", "SahminOverrideHook", function(data)
-        local owner = data.Entity
-        if not IsValid(owner) then return end
-        local sound = data.SoundName:lower()
-        local weap_start, _ = string.find(sound, "weapons/")
-        local fire_start, _ = string.find(sound, "fire")
-        local shot_start, _ = string.find(sound, "shot")
-
-        if weap_start and (fire_start or shot_start) then
-            data.SoundName = sahmin_sound
-
-            return true
-        end
-    end)
-
-    hooked = true
+    for _, wep in ipairs(client:GetWeapons()) do
+        Randomat:OverrideWeaponSound(wep, sahmin_sound)
+    end
 end)
 
 net.Receive("EndSahmin", function()
-    if IsValid(LocalPlayer()) then
-        for _, wep in pairs(LocalPlayer():GetWeapons()) do
-            if wep.Primary and wep.Primary.OriginalSound then
-                wep.Primary.Sound = wep.Primary.OriginalSound
-            end
-        end
+    hook.Remove("EntityEmitSound", "SahminOverrideHook")
+
+    if timer.Exists("RandomatSahminSoundClient") then
+        timer.Remove("RandomatSahminSoundClient")
     end
 
-    hook.Remove("EntityEmitSound", "SahminOverrideHook")
     hooked = false
+    local client = LocalPlayer()
+    if not IsValid(client) then return end
+
+    for _, wep in ipairs(client:GetWeapons()) do
+        Randomat:RestoreWeaponSound(wep)
+    end
 end)
--- lua/randomat2/events/sahmin.lua
+++ lua/randomat2/events/sahmin.lua
@@ -5,78 +5,46 @@ EVENT.id = "sahmin"
 -- Going to need to mute regular shooting sounds on the client
 util.AddNetworkString("TriggerSahmin")
 util.AddNetworkString("EndSahmin")
--- The new sounds
-util.PrecacheSound("weapons/sahmin1.mp3")
-util.PrecacheSound("weapons/sahmin2.mp3")
-util.PrecacheSound("weapons/sahmin3.mp3")
-util.PrecacheSound("weapons/sahmin4.mp3")
-util.PrecacheSound("weapons/sahmin5.mp3")
--- Client-side shooting sounds will be replaced with a silent sound file
-util.PrecacheSound("weapons/silence.mp3")
-
 -- Unlike the old 'sosig' randomat, this one plays one of many sounds
-local sahmin_sound_table = {"weapons/sahmin1.mp3", "weapons/sahmin2.mp3", "weapons/sahmin3.mp3", "weapons/sahmin4.mp3", "weapons/sahmin5.mp3"}
-
-local sahmin_sound = "weapons/silence.mp3"
-
--- Silences the regular shooting sound from a gun, on the server
-local function FixWeapon(wep)
-    if not IsValid(wep) or not wep.Primary then return end
-    wep.Primary.OriginalSound = wep.Primary.Sound
-    wep.Primary.Sound = sahmin_sound
-end
+-- local sahmin_sounds = {Sound("weapons/sahmin1.mp3"), Sound("weapons/sahmin2.mp3"), Sound("weapons/sahmin3.mp3"), Sound("weapons/sahmin4.mp3"), Sound("weapons/sahmin5.mp3")}
+-- local sahmin_index = 1
+local sahmin_sound = Sound("weapons/sahmin1.mp3")
 
 function EVENT:Begin()
-    -- Mutes regular shooting sounds on the client
+    -- timer.Create("RandomatSahminSoundServer", 1, 0, function()
+    --     sahmin_index = util.SharedRandom("RandomatSahminSound", 1, #sahmin_sounds, CurTime())
+    -- end)
     net.Start("TriggerSahmin")
     net.Broadcast()
 
-    -- Mutes regular shooting sounds on the server
-    for _, ply in pairs(self:GetPlayers()) do
-        for _, wep in pairs(ply:GetWeapons()) do
-            FixWeapon(wep)
+    for _, ply in ipairs(player.GetAll()) do
+        for _, wep in ipairs(ply:GetWeapons()) do
+            Randomat:OverrideWeaponSound(wep, sahmin_sound)
         end
     end
 
-    -- Do it again if the player obtains a new weapon
     self:AddHook("WeaponEquip", function(wep, ply)
         timer.Create("SahminDelay", 0.1, 1, function()
             net.Start("TriggerSahmin")
             net.Send(ply)
-            FixWeapon(wep)
+            Randomat:OverrideWeaponSound(wep, sahmin_sound)
         end)
     end)
 
-    -- Some weapons may still play shooting sounds, such as TFA guns, so performing a different muting method to be sure
-    self:AddHook("EntityEmitSound", function(data)
-        local owner = data.Entity
-        if not IsValid(owner) then return end
-        local sound = data.SoundName:lower()
-        local weap_start, _ = string.find(sound, "weapons/")
-        local fire_start, _ = string.find(sound, "fire")
-        local shot_start, _ = string.find(sound, "shot")
-
-        if weap_start and (fire_start or shot_start) then
-            data.SoundName = sahmin_sound
-
-            return true
-        end
-    end)
-
-    -- And finally, play the 'sahmin' sound for every bullet anything shoots
-    self:AddHook("EntityFireBullets", function(entity, data)
-        -- Play a random sound for every bullet
-        local sahmin_sound = sahmin_sound_table[math.random(1, #sahmin_sound_table)]
-        entity:EmitSound(sahmin_sound)
-    end)
+    self:AddHook("EntityEmitSound", function(data) return Randomat:OverrideWeaponSoundData(data, sahmin_sound) end)
 end
 
 function EVENT:End()
-    -- Unmute client-side gun sounds
     net.Start("EndSahmin")
     net.Broadcast()
-    -- Unmute server-side gun sounds
     timer.Remove("SahminDelay")
+    timer.Remove("RandomatSahminSoundServer")
+
+    for _, ply in ipairs(player.GetAll()) do
+        for _, wep in ipairs(ply:GetWeapons()) do
+            Randomat:RestoreWeaponSound(wep)
+        end
+    end
 end
 
 Randomat:register(EVENT)
