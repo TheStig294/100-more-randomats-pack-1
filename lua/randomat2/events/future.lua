711a0592e8786da473f46b4bc2f1c4e93977e402
The Stig <TheStig294@users.noreply.github.com>
2022-01-02 00:42:28 +1030

    Update to support all passive items, always give every item, print a centre screen message and block buying weapons more cleanly

--- lua/randomat2/events/future.lua
+++ lua/randomat2/events/future.lua
@@ -9,22 +9,49 @@ function EVENT:Begin()
     local futureRandomat = true
 
     -- Clear bought equipment table
-    for i, ply in pairs(player.GetAll()) do
+    for i, ply in ipairs(player.GetAll()) do
         futureRandomatEquipment[ply] = {}
     end
 
     -- Add bought equipment to the table and remove it from the player
-    self:AddHook("TTTOrderedEquipment", function(ply, equipment, is_item)
-        table.insert(futureRandomatEquipment[ply], equipment)
-        StripEquipment(ply, equipment, is_item)
+    self:AddHook("TTTCanOrderEquipment", function(ply, id, is_item)
+        if is_item then
+            id = math.floor(id)
+        end
+
+        if table.HasValue(futureRandomatEquipment[ply], id) then
+            ply:PrintMessage(HUD_PRINTCENTER, "You've already bought that!")
+        else
+            table.insert(futureRandomatEquipment[ply], id)
+            ply:SubtractCredits(1)
+            ply:PrintMessage(HUD_PRINTCENTER, "You'll get that on the next round")
+        end
+
+        return false
     end)
 
     -- Give everyone's bought equipment at the start of the next round
     hook.Add("TTTBeginRound", "FutureRandomatGiveEquipment", function()
         timer.Simple(0.1, function()
+            local weaponKind = 10
+
             for i, ply in pairs(player.GetAll()) do
-                for k = 1, #futureRandomatEquipment[ply] do
-                    ClassToGive(futureRandomatEquipment[ply][k], ply)
+                for j, item in ipairs(futureRandomatEquipment[ply]) do
+                    local is_item = isnumber(item)
+
+                    if is_item then
+                        ply:GiveEquipmentItem(tonumber(item))
+                    else
+                        -- Increment the weapon's kind by 1 to ensure every player receives all weapons even if some take the same slot (kind) as each other
+                        weaponKind = weaponKind + 1
+                        ply:Give(item).Kind = weaponKind
+
+                        if item.WasBought then
+                            item:WasBought(ply)
+                        end
+                    end
+
+                    Randomat:CallShopHooks(is_item, item, ply)
                 end
             end
         end)
