29ce68b934beeebf744a527ee4b742ff5988da8e
The Stig <TheStig294@users.noreply.github.com>
2022-01-01 22:54:15 +1030

    Much more cleanly prevents equipment from being bought, added chat message

--- lua/randomat2/events/mix.lua
+++ lua/randomat2/events/mix.lua
@@ -7,44 +7,43 @@ function EVENT:Begin()
     -- TTT by default uses 8 different slots for weapons, if you're using TTT2 or something that adds more weapon slots where slots 100 and above become meaningful, this might cause unexpected behaviour... (But the randomat event itself should still work)
     local wepSlot = 100
 
-    -- Whenever someone buys something, 
-    self:AddHook("TTTOrderedEquipment", function(ply, equipment, is_item)
-        -- Remove what they bought
-        StripEquipment(ply, equipment, is_item)
+    self:AddHook("TTTCanOrderEquipment", function(ply, id, is_item)
+        if not IsValid(ply) then return end
+        -- Get all weapons
+        local weps = weapons.GetList()
+        local playerWeps = ply:GetWeapons()
+        local give_weps = {}
 
-        -- Wait a split second
-        timer.Simple(0, function()
-            -- Get all weapons
-            local weps = weapons.GetList()
-            local playerWeps = ply:GetWeapons()
-            local give_weps = {}
+        -- Filter out the un-buyable weapons
+        for i = 1, #weps do
+            local wep = weps[i]
+            local weaponOwned = false
 
-            -- Filter out the un-buyable weapons
-            for i = 1, #weps do
-                local wep = weps[i]
-                local weaponOwned = false
-
-                -- Filter out all the weapons the player already owns
-                for j, wepPly in ipairs(playerWeps) do
-                    if wepPly == wep then
-                        weaponOwned = true
-                    end
-                end
-
-                if wep and (wep.CanBuy and #wep.CanBuy > 0) and not weaponOwned then
-                    give_weps[#give_weps + 1] = wep
+            -- Filter out all the weapons the player already owns
+            for j, wepPly in ipairs(playerWeps) do
+                if wepPly == wep then
+                    weaponOwned = true
                 end
             end
 
-            -- Select a random weapon and give it to the player
-            if not table.IsEmpty(give_weps) then
-                local item = give_weps[math.random(#give_weps)]
-                -- Override the weapon's slot so one is always given
-                item.Kind = wepSlot
-                ply:Give(WEPS.GetClass(item))
-                wepSlot = wepSlot + 1
+            if wep and (wep.CanBuy and #wep.CanBuy > 0) and not weaponOwned then
+                give_weps[#give_weps + 1] = wep
             end
-        end)
+        end
+
+        -- Select a random weapon and give it to the player
+        if not table.IsEmpty(give_weps) then
+            local item = give_weps[math.random(#give_weps)]
+            -- Override the weapon's slot so one is always given
+            item.Kind = wepSlot
+            ply:Give(WEPS.GetClass(item))
+            wepSlot = wepSlot + 1
+        end
+
+        ply:SubtractCredits(1)
+        ply:ChatPrint("It's '" .. Randomat:GetEventTitle(EVENT) .. "'\nYou got a random item.")
+
+        return false
     end)
 end
 
