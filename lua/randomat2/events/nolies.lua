8ea512e5797ee7e1432202ddf4b656d65942bab7
The Stig <TheStig294@users.noreply.github.com>
2022-01-24 12:29:17 +1030

    Fixed formatting and changed to PostPlayerDeath

--- lua/randomat2/events/nolies.lua
+++ lua/randomat2/events/nolies.lua
@@ -4,9 +4,8 @@ EVENT.Description = "Reveals a player's role on death"
 EVENT.id = "nolies"
 
 function EVENT:Begin()
-    self:AddHook("PlayerDeath", function(victim, inflictor, attacker)
-        -- GetRoleName() is a base randomat function, found in randomat_base_stig.lua
-        self:SmallNotify(self:GetRoleName(victim) .. " Has Died")
+    self:AddHook("PostPlayerDeath", function(ply)
+        self:SmallNotify(self:GetRoleName(ply) .. " has died")
     end)
 end
 
