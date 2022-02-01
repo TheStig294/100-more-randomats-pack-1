3cac685a61932a8d7fc4d7c15bea7a3b04916b7e
The Stig <TheStig294@users.noreply.github.com>
2022-02-01 16:13:55 +1030

    Cleanup and added [PLAYER] WINS! win message

---
+++ lua/randomat2/cl_events/cl_battleroayle.lua
@@ -0,0 +1,26 @@
+net.Receive("RandomatBattleRoyaleWinTitle", function()
+    hook.Add("TTTScoringWinTitle", "RandomatBattleRoyalWinTitle", function(wintype, wintitles, title)
+        local winner
+
+        for i, ply in ipairs(player.GetAll()) do
+            if ply:Alive() and not ply:IsSpec() then
+                winner = ply
+            end
+        end
+
+        if not winner then return end
+        LANG.AddToLanguage("english", "win_battleroyale", string.upper(winner:Nick() .. " wins!"))
+
+        local newTitle = {
+            txt = "win_battleroyale",
+            c = COLOR_INNOCENT["default"],
+            params = nil
+        }
+
+        return newTitle
+    end)
+end)
+
+net.Receive("RandomatBattleRoyaleWinTitleRemove", function()
+    hook.Remove("TTTScoringWinTitle", "RandomatBattleRoyalWinTitle")
+end)
--- lua/randomat2/events/battleroyale.lua
+++ lua/randomat2/events/battleroyale.lua
@@ -6,6 +6,8 @@ EVENT.Title = "LAST ONE STANDING WINS! BATTLE ROYALE!"
 EVENT.Description = ""
 EVENT.id = "battleroyale"
 local alertSound = Sound("battleroyale/alert.mp3")
+util.AddNetworkString("RandomatBattleRoyaleWinTitle")
+util.AddNetworkString("RandomatBattleRoyaleWinTitleRemove")
 
 function EVENT:Begin()
     local fortniteToolExists = weapons.Get("weapon_ttt_fortnite_building") ~= nil
@@ -14,17 +16,17 @@ function EVENT:Begin()
         EVENT.Description = "Press 'F' to change platform shape"
     end
 
-    --Plays the Fortnite alert sound as an extra warning this randomat has started
+    -- Plays the Fortnite alert sound as an extra warning this randomat has started
     for i, ply in pairs(self:GetPlayers()) do
         ply:EmitSound(alertSound)
     end
 
-    --Preventing the round from ending if more than 1 person is alive
+    -- Preventing the round from ending if more than 1 person is alive
     self:AddHook("TTTCheckForWin", function()
         if #self:GetAlivePlayers() > 1 then return WIN_NONE end
     end)
 
-    --After the set amount of time runs out, (default 2 minutes) everyone is given a radar to prevent camping
+    -- After the set amount of time runs out, (default 2 minutes) everyone is given a radar to prevent camping
     timer.Create("BattleRoyaleRandomatTimer", 1, GetConVar("randomat_battleroyale_radar_time"):GetInt(), function()
         if timer.RepsLeft("BattleRoyaleRandomatTimer") == 0 then
             self:SmallNotify("The circle is shrinking! (Radar activated)")
@@ -38,10 +40,10 @@ function EVENT:Begin()
         end
     end)
 
-    --Disabling lowering somone's karma for shooting a fellow innocent, as everyone will be innocent
+    -- Disabling giving karma penalties
     self:AddHook("TTTKarmaGivePenalty", function(ply, penalty, victim) return true end)
 
-    --Giving everyone a Fortnite building tool, if one of its convars is found, and turning everyone into an innocent
+    -- Giving everyone a Fortnite building tool, if one of its convars is found, and turning everyone into an innocent
     for i, ply in pairs(self:GetAlivePlayers()) do
         timer.Simple(0.1, function()
             if fortniteToolExists then
@@ -54,11 +56,22 @@ function EVENT:Begin()
             SendFullStateUpdate()
         end)
     end
+
+    if CR_VERSION then
+        net.Start("RandomatBattleRoyaleWinTitle")
+        net.Broadcast()
+    end
 end
 
---Removing the radar timer, else everyone will be given a radar next round...
 function EVENT:End()
+    -- Removing the radar timer, else everyone will be given a radar next round...
     timer.Remove("BattleRoyaleRandomatTimer")
+
+    -- Also remove the win title hook, else the win title will always be "[Player] WINS!"
+    if CR_VERSION then
+        net.Start("RandomatBattleRoyaleWinTitleRemove")
+        net.Broadcast()
+    end
 end
 
 function EVENT:GetConVars()
