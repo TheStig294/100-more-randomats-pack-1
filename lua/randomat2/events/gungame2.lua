08ef86522dcab38d4eca0a8bef1157767560e1dc
The Stig <TheStig294@users.noreply.github.com>
2022-02-02 23:18:22 +1030

    Cleanup, gives gun on respawning, cannot drop guns

--- lua/randomat2/events/gungame2.lua
+++ lua/randomat2/events/gungame2.lua
@@ -5,6 +5,34 @@ EVENT.id = "gungame2"
 -- Declares this randomat a 'Weapon Override' randomat, meaning it cannot trigger if another Weapon Override randomat has triggered in the round
 EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
 
+local function GiveNewWeapon(ply, weps)
+    -- See if they are currently holding a weapon that will be swapped
+    local holdingFloorWeapon = false
+
+    if ply:GetActiveWeapon().Kind == WEAPON_HEAVY or ply:GetActiveWeapon().Kind == WEAPON_PISTOL then
+        holdingFloorWeapon = true
+    end
+
+    -- Remove their pistol and main gun
+    for _, wep in ipairs(ply:GetWeapons()) do
+        if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
+            ply:StripWeapon(wep.ClassName)
+        end
+    end
+
+    -- Give them a random weapon
+    local wepGiven = table.Random(weps)
+    local wep = ply:Give(wepGiven.ClassName)
+    wep.AllowDrop = false
+    -- Reset FOV to unscope
+    ply:SetFOV(0, 0.2)
+
+    -- If they were holding a removed weapon, force them to select the new one
+    if holdingFloorWeapon then
+        ply:SelectWeapon(wepGiven.ClassName)
+    end
+end
+
 function EVENT:Begin()
     -- Remove all weapons from the ground, except grenades
     for _, ent in ipairs(ents.GetAll()) do
@@ -23,63 +51,24 @@ function EVENT:Begin()
         end
     end
 
-    -- Give everyone their first weapon
-    for _, v in ipairs(player.GetAll()) do
-        -- See if they are currently holding a weapon that will be swapped
-        local ac = false
-
-        if v:GetActiveWeapon().Kind == WEAPON_HEAVY or v:GetActiveWeapon().Kind == WEAPON_PISTOL then
-            ac = true
-        end
-
-        -- Remove their pistol and main gun
-        for _, wep in ipairs(v:GetWeapons()) do
-            if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
-                v:StripWeapon(wep.ClassName)
-            end
-        end
-
-        -- Give them a random weapon
-        local wepGiven = table.Random(weps)
-        v:Give(wepGiven.ClassName)
-        -- Reset FOV to unscope
-        v:SetFOV(0, 0.2)
-
-        -- If they were holding a removed weapon, force them to select the new one
-        if ac then
-            v:SelectWeapon(wepGiven.ClassName)
-        end
+    -- Give everyone their initial weapon
+    for _, v in ipairs(self:GetAlivePlayers()) do
+        GiveNewWeapon(v, weps)
     end
 
     -- Give everyone a new weapon when someone dies
     self:AddHook("PlayerDeath", function(victim, inflictor, attacker)
         timer.Simple(0.1, function()
             for _, v in ipairs(player.GetAll()) do
-                -- See if they are currently holding a weapon that will be swapped
-                local ac = false
-
-                if v:GetActiveWeapon().Kind == WEAPON_HEAVY or v:GetActiveWeapon().Kind == WEAPON_PISTOL then
-                    ac = true
-                end
-
-                -- Remove their pistol and main gun
-                for _, wep in ipairs(v:GetWeapons()) do
-                    if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
-                        v:StripWeapon(wep.ClassName)
-                    end
-                end
-
-                -- Give them a random weapon
-                local wepGiven = table.Random(weps)
-                v:Give(wepGiven.ClassName)
-                -- Reset FOV to unscope
-                v:SetFOV(0, 0.2)
-
-                -- If they were holding a removed weapon, force them to select the new one
-                if ac then
-                    v:SelectWeapon(wepGiven.ClassName)
-                end
+                GiveNewWeapon(v, weps)
             end
+        end)
+    end)
+
+    -- Give a new weapon when someone respawns
+    self:AddHook("PlayerSpawn", function(ply)
+        timer.Simple(0.1, function()
+            GiveNewWeapon(ply, weps)
         end)
     end)
 end
