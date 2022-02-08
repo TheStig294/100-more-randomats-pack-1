4beb8200582c0f5e259df109aff0ec14919bdd58
The Stig <TheStig294@users.noreply.github.com>
2022-02-08 14:15:30 +1030

    Now changes anyone with boba's name or playermodel to a solid snake model if installed

--- lua/randomat2/events/speedrun.lua
+++ lua/randomat2/events/speedrun.lua
@@ -8,14 +8,17 @@ EVENT.id = "speedrun"
 local speedrunRandomat = false
 local hasteMode = false
 local hasteMinutes = 0.5
+local standardHeightVector = Vector(0, 0, 64)
+local standardCrouchedHeightVector = Vector(0, 0, 28)
+local playerModels = {}
+local modelExists = util.IsValidModel("models/vinrax/player/mgs_solid_snake.mdl")
 
 function EVENT:Begin()
     speedrunRandomat = true
     hasteMode = GetConVar("ttt_haste"):GetBool()
     hasteMinutes = GetConVar("ttt_haste_minutes_per_death"):GetFloat()
-
     local time = GetConVar("randomat_speedrun_time"):GetInt()
-    self.Description =  time .. " second round"
+    self.Description = time .. " second round"
 
     -- This global float controls the time the round will end, part of base TTT
     if hasteMode then
@@ -25,6 +28,32 @@ function EVENT:Begin()
         SetGlobalFloat("ttt_round_end", CurTime() + time)
     else
         SetGlobalFloat("ttt_round_end", CurTime() + time)
+    end
+
+    if modelExists then
+        for i, ply in ipairs(self:GetAlivePlayers()) do
+            if ply:GetModel() == "models/bna/michiru.mdl" or ply:Nick() == "boba" then
+                if (not ply:IsBot()) then
+                    -- We need to disable cl_playermodel_selector_force, because it messes with SetModel, we'll reset it when the event ends
+                    ply:ConCommand("cl_playermodel_selector_force 0")
+                end
+
+                -- we need  to wait a second for cl_playermodel_selector_force to take effect (and THEN change model)
+                timer.Simple(1, function()
+                    -- if the player's viewoffset is different than the standard, then...
+                    if ply:GetViewOffset() ~= standardHeightVector then
+                        -- So we set their new heights to the default values (which the Duncan model uses)
+                        ply:SetViewOffset(standardHeightVector)
+                        ply:SetViewOffsetDucked(standardCrouchedHeightVector)
+                    end
+
+                    playerModels[ply] = ply:GetModel()
+                    -- Sets their model to chosenModel
+                    ply:SetModel("models/vinrax/player/mgs_solid_snake.mdl")
+                    ply:ChatPrint("You share the same name or playermodel as a world-famous speedrunner.\nYour playermodel has changed for this round...")
+                end)
+            end
+        end
     end
 end
 
@@ -37,6 +66,25 @@ function EVENT:End()
 
         -- And prevent the end function from being run until this randomat triggers again
         speedrunRandomat = false
+
+        if modelExists then
+            -- loop through all players
+            for k, ply in pairs(player.GetAll()) do
+                -- if the index k in the table playermodels has a model, then...
+                if (playerModels[ply] ~= nil) then
+                    -- we set the player ply to the playermodel with index k in the table
+                    -- this should invoke the viewheight script from the models and fix viewoffsets (e.g. Zoey's model) 
+                    -- this does however first reset their viewmodel in the preparing phase (when they respawn)
+                    -- might be glitchy with pointshop items that allow you to get a viewoffset
+                    ply:SetModel(playerModels[ply])
+                end
+
+                -- we reset the cl_playermodel_selector_force to 1, otherwise TTT will reset their playermodels on a new round start (to default models!)
+                ply:ConCommand("cl_playermodel_selector_force 1")
+                -- clear the model table to avoid setting wrong models (e.g. disconnected players)
+                table.Empty(playerModels)
+            end
+        end
     end
 end
 
