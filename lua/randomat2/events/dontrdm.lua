2b1aa54ef090d7da4b55211dff3a1227c8370f3a
The Stig <TheStig294@users.noreply.github.com>
2022-02-15 13:26:15 +1030

    Banned the defib during rdm randomats

--- lua/randomat2/events/dontrdm.lua
+++ lua/randomat2/events/dontrdm.lua
@@ -54,6 +54,38 @@ function EVENT:Begin()
             end)
         end
     end)
+
+    -- Banning the defib from being used during this randomat
+    local defibClassnames = {"weapon_detective_defib", "weapon_vadim_defib", "weapon_ttt_defib"}
+
+    -- Removing held defibs
+    for _, ply in ipairs(self:GetAlivePlayers()) do
+        for _, classname in ipairs(defibClassnames) do
+            if ply:HasWeapon(classname) then
+                ply:StripWeapon(classname)
+                ply:AddCredits(1)
+
+                timer.Simple(1, function()
+                    ply:ChatPrint("Defibrillators are disabled while '" .. Randomat:GetEventTitle(EVENT) .. "' is active!\nYour credit has been refunded.")
+                end)
+            end
+        end
+    end
+
+    -- Preventing defibs from being bought
+    self:AddHook("TTTCanOrderEquipment", function(ply, id, is_item)
+        if IsPlayer(ply) and Randomat:IsInnocentTeam(ply) and not is_item then
+            for _, classname in ipairs(defibClassnames) do
+                if id == classname then
+                    timer.Simple(1, function()
+                        ply:ChatPrint("Defibrillators are disabled while '" .. Randomat:GetEventTitle(EVENT) .. "' is active!")
+                    end)
+
+                    return false
+                end
+            end
+        end
+    end)
 end
 
 Randomat:register(EVENT)
