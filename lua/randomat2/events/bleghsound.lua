4570fb299c4a6f8304fe415bbffe212012902f75
The Stig <TheStig294@users.noreply.github.com>
2022-04-09 01:07:54 +0930

    Changes to 'Blegh' event: - Changed description when using sharky model - Sharky model data is now delcared in-line - The normal sharky model is replaced with the ben face one - Everyone now gets a unique model, until all are used - Added rainbow sharky that changes colours over time

--- lua/randomat2/events/bleghsound.lua
+++ lua/randomat2/events/bleghsound.lua
@@ -4,7 +4,7 @@ EVENT.Description = "Everyone hears a 'Blegh!' sound when someone dies"
 local plushSharkModel = "models/bradyjharty/yogscast/sharky.mdl"
 
 if util.IsValidModel(plushSharkModel) then
-    EVENT.Description = "Make a 'Blegh!' sound when you die, everyone is sharky!"
+    EVENT.Description = "Make a 'Blegh!' sound when you die, everyone is a different sharky!"
 end
 
 EVENT.id = "bleghsound"
@@ -26,134 +26,236 @@ function EVENT:Begin()
         net.Broadcast()
     end)
 
-    -- Gives everyone a random sharky playermodel, if installed
-    local plushSharkOffset = Vector(0, 0, 40)
-    local plushSharkOffsetDucked = Vector(0, 0, 28)
-    local playermodelSets = {}
-
     if util.IsValidModel(plushSharkModel) then
-        -- Space marine sharky
-        local sharky1 = {}
-        sharky1.model = plushSharkModel
-        sharky1.viewOffset = plushSharkOffset
-        sharky1.viewOffsetDucked = plushSharkOffsetDucked
-        sharky1.skin = 3
-        sharky1.bodygroupValues = {}
-        sharky1.playerColor = Color(5, 64, 140):ToVector()
-        sharky1.bodygroupValues[0] = 0
-        sharky1.bodygroupValues[1] = 1
-        sharky1.bodygroupValues[2] = 0
-        sharky1.bodygroupValues[3] = 0
-        sharky1.bodygroupValues[4] = 0
-        sharky1.bodygroupValues[5] = 1
-        sharky1.bodygroupValues[6] = 1
-        table.insert(playermodelSets, sharky1)
-        -- Christmas Sharky
-        local sharky2 = {}
-        sharky2.model = plushSharkModel
-        sharky2.viewOffset = plushSharkOffset
-        sharky2.viewOffsetDucked = plushSharkOffsetDucked
-        sharky2.playerColor = Color(176, 33, 33):ToVector()
-        sharky2.skin = 1
-        sharky2.bodygroupValues = {}
-        sharky2.bodygroupValues[0] = 0
-        sharky2.bodygroupValues[1] = 4
-        sharky2.bodygroupValues[2] = 2
-        sharky2.bodygroupValues[3] = 1
-        sharky2.bodygroupValues[4] = 0
-        sharky2.bodygroupValues[5] = 0
-        sharky2.bodygroupValues[6] = 0
-        table.insert(playermodelSets, sharky2)
-        -- Drunk lifevest sharky
-        local sharky3 = {}
-        sharky3.model = plushSharkModel
-        sharky3.viewOffset = plushSharkOffset
-        sharky3.viewOffsetDucked = plushSharkOffsetDucked
-        sharky3.playerColor = Color(61, 87, 105):ToVector()
-        sharky3.skin = 5
-        sharky3.bodygroupValues = {}
-        sharky3.bodygroupValues[0] = 0
-        sharky3.bodygroupValues[1] = 2
-        sharky3.bodygroupValues[2] = 0
-        sharky3.bodygroupValues[3] = 1
-        sharky3.bodygroupValues[4] = 0
-        sharky3.bodygroupValues[5] = 0
-        sharky3.bodygroupValues[6] = 0
-        table.insert(playermodelSets, sharky3)
-        -- Pirate Sharky
-        local sharky4 = {}
-        sharky4.model = plushSharkModel
-        sharky4.viewOffset = plushSharkOffset
-        sharky4.viewOffsetDucked = plushSharkOffsetDucked
-        sharky4.playerColor = Color(109, 109, 109):ToVector()
-        sharky4.skin = 3
-        sharky4.bodygroupValues = {}
-        sharky4.bodygroupValues[0] = 0
-        sharky4.bodygroupValues[1] = 3
-        sharky4.bodygroupValues[2] = 1
-        sharky4.bodygroupValues[3] = 2
-        sharky4.bodygroupValues[4] = 0
-        sharky4.bodygroupValues[5] = 0
-        sharky4.bodygroupValues[6] = 0
-        table.insert(playermodelSets, sharky4)
-        -- Ben in mouth
-        local sharky5 = {}
-        sharky5.model = plushSharkModel
-        sharky5.viewOffset = plushSharkOffset
-        sharky5.viewOffsetDucked = plushSharkOffsetDucked
-        sharky5.skin = 0
-        sharky5.bodygroupValues = {}
-        sharky5.bodygroupValues[0] = 0
-        sharky5.bodygroupValues[1] = 0
-        sharky5.bodygroupValues[2] = 0
-        sharky5.bodygroupValues[3] = 1
-        sharky5.bodygroupValues[4] = 1
-        sharky5.bodygroupValues[5] = 0
-        sharky5.bodygroupValues[6] = 0
-        table.insert(playermodelSets, sharky5)
-        -- OG Plush Sharky
-        local sharky6 = {}
-        sharky6.model = plushSharkModel
-        sharky6.viewOffset = plushSharkOffset
-        sharky6.viewOffsetDucked = plushSharkOffsetDucked
-        sharky6.skin = 0
-        sharky6.bodygroupValues = {}
-        sharky6.bodygroupValues[0] = 0
-        sharky6.bodygroupValues[1] = 0
-        sharky6.bodygroupValues[2] = 0
-        sharky6.bodygroupValues[3] = 0
-        sharky6.bodygroupValues[4] = 0
-        sharky6.bodygroupValues[5] = 0
-        sharky6.bodygroupValues[6] = 0
-        table.insert(playermodelSets, sharky6)
+        -- Gives everyone a random sharky playermodel, if installed
+        local plushSharkOffset = Vector(0, 0, 40)
+        local plushSharkOffsetDucked = Vector(0, 0, 28)
+        local playerModelSets = {}
 
-        -- OG Left Shark
+        local spaceMarineSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            skin = 3,
+            playerColor = Color(5, 64, 140):ToVector(),
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 1,
+                [2] = 0,
+                [3] = 0,
+                [4] = 0,
+                [5] = 1,
+                [6] = 1,
+            }
+        }
+
+        table.insert(playerModelSets, spaceMarineSharky)
+
+        local christmasSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            skin = 1,
+            playerColor = Color(176, 33, 33):ToVector(),
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 4,
+                [2] = 2,
+                [3] = 1,
+                [4] = 0,
+                [5] = 0,
+                [6] = 0
+            }
+        }
+
+        table.insert(playerModelSets, christmasSharky)
+
+        local lifevestSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            playerColor = Color(61, 87, 105):ToVector(),
+            skin = 5,
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 2,
+                [2] = 0,
+                [3] = 1,
+                [4] = 0,
+                [5] = 0,
+                [6] = 0
+            }
+        }
+
+        table.insert(playerModelSets, lifevestSharky)
+
+        local pirateSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            playerColor = Color(109, 109, 109):ToVector(),
+            skin = 3,
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 3,
+                [2] = 1,
+                [3] = 2,
+                [4] = 0,
+                [5] = 0,
+                [6] = 0
+            }
+        }
+
+        table.insert(playerModelSets, pirateSharky)
+
+        local benSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            skin = 0,
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 0,
+                [2] = 0,
+                [3] = 0,
+                [4] = 1,
+                [5] = 0,
+                [6] = 0
+            }
+        }
+
+        table.insert(playerModelSets, benSharky)
+
+        local rainbowSharky = {
+            model = plushSharkModel,
+            viewOffset = plushSharkOffset,
+            viewOffsetDucked = plushSharkOffsetDucked,
+            playerColor = Color(255, 0, 0):ToVector(),
+            skin = 1,
+            bodygroupValues = {
+                [0] = 0,
+                [1] = 0,
+                [2] = 0,
+                [3] = 0,
+                [4] = 0,
+                [5] = 0,
+                [6] = 0
+            }
+        }
+
+        table.insert(playerModelSets, rainbowSharky)
+
+        -- The original left shark playermodel can also be given, if it's installed as well
         if util.IsValidModel("models/freeman/player/left_shark.mdl") then
-            local sharky7 = {}
-            sharky7.model = "models/freeman/player/left_shark.mdl"
-            sharky7.skin = 1
-            table.insert(playermodelSets, sharky7)
+            local ogLeftShark = {
+                model = "models/freeman/player/left_shark.mdl",
+                skin = 1
+            }
+
+            table.insert(playerModelSets, ogLeftShark)
         end
 
-        local playermodelIDs = {}
+        -- Randomly assign unique playermodels to everyone
+        local remainingPlayermodels = {}
+        local chosenPlayermodels = {}
+        table.Add(remainingPlayermodels, playerModelSets)
 
         for _, ply in ipairs(self:GetAlivePlayers()) do
-            local playermodelID = math.random(1, #playermodelSets)
-            playermodelIDs[ply] = playermodelID
-            ForceSetPlayermodel(ply, playermodelSets[playermodelID])
+            -- But if all playermodels have been used, reset the pool of playermodels
+            if table.IsEmpty(remainingPlayermodels) then
+                table.Add(remainingPlayermodels, playerModelSets)
+            end
+
+            local modelData = table.Random(remainingPlayermodels)
+            ForceSetPlayermodel(ply, modelData)
+
+            if modelData == rainbowSharky then
+                ply:SetNWBool("RainbowSharkyRandomat", true)
+            end
+
+            -- Remove the selected model from the pool
+            table.RemoveByValue(remainingPlayermodels, modelData)
+            -- Keep track of who got what model so it can be set when they respawn
+            chosenPlayermodels[ply] = modelData
         end
 
-        -- Sets someone's playermodel again when respawning
+        -- Sets someone's playermodel again when respawning,
+        -- if they weren't in the round when the event triggered, set their chosen model to a random one
         self:AddHook("PlayerSpawn", function(ply)
+            if not chosenPlayermodels[ply] then
+                chosenPlayermodels[ply] = table.Random(playerModelSets)
+            end
+
             timer.Simple(1, function()
-                ForceSetPlayermodel(ply, playermodelSets[playermodelIDs[ply]])
+                ForceSetPlayermodel(ply, chosenPlayermodels[ply])
             end)
+        end)
+
+        -- Rainbow Sharky logic to change colours over time
+        local rainbowPhase = 1
+
+        self:AddHook("Think", function()
+            for _, ply in ipairs(self:GetAlivePlayers()) do
+                if ply:GetNWBool("RainbowSharkyRandomat") then
+                    local vector = ply:GetPlayerColor()
+
+                    if rainbowPhase == 1 then
+                        vector.z = vector.z + (1 / 128)
+
+                        if vector.z + (1 / 128) > 1 then
+                            vector.z = 1
+                            rainbowPhase = rainbowPhase + 1
+                        end
+                    elseif rainbowPhase == 2 then
+                        vector.x = vector.x - (1 / 128)
+
+                        if vector.x - (1 / 128) < 0 then
+                            vector.x = 0
+                            rainbowPhase = rainbowPhase + 1
+                        end
+                    elseif rainbowPhase == 3 then
+                        vector.y = vector.y + (1 / 128)
+
+                        if vector.y + (1 / 128) > 1 then
+                            vector.y = 1
+                            rainbowPhase = rainbowPhase + 1
+                        end
+                    elseif rainbowPhase == 4 then
+                        vector.z = vector.z - (1 / 128)
+
+                        if vector.z - (1 / 128) < 0 then
+                            vector.z = 0
+                            rainbowPhase = rainbowPhase + 1
+                        end
+                    elseif rainbowPhase == 5 then
+                        vector.x = vector.x + (1 / 128)
+
+                        if vector.x + (1 / 128) > 1 then
+                            vector.x = 1
+                            rainbowPhase = rainbowPhase + 1
+                        end
+                    elseif rainbowPhase == 6 then
+                        vector.y = vector.y - (1 / 128)
+
+                        if vector.y - (1 / 128) < 0 then
+                            vector.y = 0
+                            rainbowPhase = 1
+                        end
+                    end
+
+                    ply:SetPlayerColor(vector)
+                end
+            end
         end)
     end
 end
 
 function EVENT:End()
     ForceResetAllPlayermodels()
+
+    for _, ply in ipairs(player.GetAll()) do
+        ply:SetNWBool("RainbowSharkyRandomat", false)
+    end
 end
 
 Randomat:register(EVENT)
