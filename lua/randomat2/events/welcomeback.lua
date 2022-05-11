989cc62b7d3f1fbefbf6ee42b79f744304b36431
The Stig <TheStig294@users.noreply.github.com>
2022-05-11 22:02:55 +0930

    Added player name introductions

--- lua/randomat2/events/welcomeback.lua
+++ lua/randomat2/events/welcomeback.lua
@@ -15,9 +15,37 @@ function EVENT:Begin()
     net.Start("WelcomeBackRandomatPopup")
     net.WriteString(randomIntroSound)
     net.Broadcast()
+    local alivePlayers = self:GetAlivePlayers(true)
+    local playerCount = #alivePlayers
+    local maxPlayersAnnounced = 7
+
+    -- Displays this event's name in full and introduces up to 8 players names through randomat alerts
+    timer.Create("WelcomeBackAlertStart", 3.031, 1, function()
+        Randomat:EventNotify("Hello everyone and welcome back to Trouble in Terrorist Town!")
+
+        timer.Create("WelcomeBackPlayerNameStart", 5, 1, function()
+            self:SmallNotify("Today we're joined by...", 3)
+
+            timer.Create("WelcomeBackIntroducePlayers", 3, playerCount, function()
+                local namedPlayerCount = playerCount - timer.RepsLeft("WelcomeBackIntroducePlayers")
+
+                -- Display the player's name if we're not over the cap on the amount of names displayed, and not on the last player
+                if namedPlayerCount < maxPlayersAnnounced + 1 and namedPlayerCount ~= playerCount - 1 then
+                    self:SmallNotify(alivePlayers[namedPlayerCount]:Nick() .. " and...", 3)
+                else
+                    -- But make the last alert always display "...did I forget anyone?" and not display that player's name
+                    self:SmallNotify("...did I forget anyone?", 4)
+                    timer.Remove("WelcomeBackIntroducePlayers")
+                end
+            end)
+        end)
+    end)
 end
 
 function EVENT:End()
+    timer.Remove("WelcomeBackAlertStart")
+    timer.Remove("WelcomeBackPlayerNameStart")
+    timer.Remove("WelcomeBackIntroducePlayers")
 end
 
 Randomat:register(EVENT)
