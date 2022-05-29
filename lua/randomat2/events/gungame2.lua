78da93bf20ec378e94035f0c154fa1285760b5bd
The Stig <TheStig294@users.noreply.github.com>
2022-05-29 14:28:33 +0930

    'Gun Game 2.0' now gives ammo along with guns

--- lua/randomat2/events/gungame2.lua
+++ lua/randomat2/events/gungame2.lua
@@ -9,6 +9,27 @@ EVENT.Categories = {"deathtrigger", "biased_innocent", "biased", "largeimpact"}
 
 local weps = {}
 
+-- Gives ammo to a player's gun equivalent to ammo boxes, without going over TTT's reserve ammo limits
+local function GiveTTTAmmoBoxesForGun(ply, className, boxNumber)
+    local SWEP = weapons.Get(className)
+    local ammoEnt = SWEP.AmmoEnt
+    local ammoType = SWEP.Primary.Ammo
+
+    if ammoEnt then
+        if ammoEnt == "item_ammo_pistol_ttt" then
+            ply:SetAmmo(math.min(60, ply:GetAmmoCount(ammoType) + 20 * boxNumber), ammoType)
+        elseif ammoEnt == "item_ammo_smg1_ttt" then
+            ply:SetAmmo(math.min(60, ply:GetAmmoCount(ammoType) + 30 * boxNumber), ammoType)
+        elseif ammoEnt == "item_ammo_revolver_ttt" then
+            ply:SetAmmo(math.min(36, ply:GetAmmoCount(ammoType) + 12 * boxNumber), ammoType)
+        elseif ammoEnt == "item_ammo_357_ttt" then
+            ply:SetAmmo(math.min(20, ply:GetAmmoCount(ammoType) + 10 * boxNumber), ammoType)
+        elseif ammoEnt == "item_box_buckshot_ttt" then
+            ply:SetAmmo(math.min(24, ply:GetAmmoCount(ammoType) + 8 * boxNumber), ammoType)
+        end
+    end
+end
+
 local function GiveNewWeapon(ply)
     -- See if they are currently holding a weapon that will be swapped
     local holdingFloorWeapon = false
@@ -40,6 +61,8 @@ local function GiveNewWeapon(ply)
     if holdingFloorWeapon then
         ply:SelectWeapon(wepGiven)
     end
+
+    GiveTTTAmmoBoxesForGun(ply, wepGiven, 2)
 end
 
 function EVENT:Begin()
