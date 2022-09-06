6b7a64ae065c572065ee311f7583f1ce54f59ac1
The Stig <TheStig294@users.noreply.github.com>
2022-09-06 15:22:08 +0930

    'The Bucket' now spawns the bucket on top of a player when there are no find spawns to fix this event not doing anything when manually triggered

--- lua/randomat2/events/bucket.lua
+++ lua/randomat2/events/bucket.lua
@@ -8,11 +8,47 @@ EVENT.Categories = {"entityspawn", "moderateimpact"}
 
 util.AddNetworkString("BucketRandomatOutline")
 util.AddNetworkString("BucketRandomatOutlineEnd")
-local possibleSpawns = {}
 
 function EVENT:Begin()
-    local ent = possibleSpawns[math.random(1, #possibleSpawns)]
-    local pos = ent:GetPos()
+    local possibleSpawns = {}
+    -- Get every player's position so the bucket isn't spawned too close to a player
+    local playerPositions = {}
+
+    for _, ply in ipairs(self:GetAlivePlayers()) do
+        table.insert(playerPositions, ply:GetPos())
+    end
+
+    for _, ent in ipairs(ents.GetAll()) do
+        local classname = ent:GetClass()
+        local pos = ent:GetPos()
+
+        -- Using the positions of weapon, ammo and player spawns
+        if (string.StartWith(classname, "info_")) and not IsValid(ent:GetParent()) then
+            local tooClose = false
+
+            for _, plyPos in ipairs(playerPositions) do
+                -- 100 * 100 = 10,000, so any spot closer than 100 source units to the player is too close to be placed
+                if math.DistanceSqr(pos.x, pos.y, plyPos.x, plyPos.y) < 10000 then
+                    tooClose = true
+                    break
+                end
+            end
+
+            if not tooClose then
+                table.insert(possibleSpawns, ent:GetPos())
+            end
+        end
+    end
+
+    if table.IsEmpty(possibleSpawns) or possibleSpawns == {} then
+        possibleSpawns = playerPositions
+
+        timer.Simple(5, function()
+            self:SmallNotify("No possible bucket spawns! Spawned on top of a player!")
+        end)
+    end
+
+    local pos = possibleSpawns[math.random(1, #possibleSpawns)]
     local bucket = ents.Create("ent_bucket_randomat")
     bucket:SetPos(pos + Vector(0, 0, 10))
     bucket:Spawn()
@@ -45,40 +81,6 @@ function EVENT:End()
     timer.Remove("BucketRandomatOutlineDelay")
     net.Start("BucketRandomatOutlineEnd")
     net.Broadcast()
-end
-
--- Get every player's position so the bucket isn't spawned too close to a player
-function EVENT:Condition()
-    possibleSpawns = {}
-    local playerPositions = {}
-
-    for _, ply in ipairs(self:GetAlivePlayers()) do
-        table.insert(playerPositions, ply:GetPos())
-    end
-
-    for _, ent in ipairs(ents.GetAll()) do
-        local classname = ent:GetClass()
-        local pos = ent:GetPos()
-
-        -- Using the positions of weapon, ammo and player spawns
-        if (string.StartWith(classname, "info_")) and not IsValid(ent:GetParent()) then
-            local tooClose = false
-
-            for _, plyPos in ipairs(playerPositions) do
-                -- 100 * 100 = 10,000, so any spot closer than 100 source units to the player is too close to be placed
-                if math.DistanceSqr(pos.x, pos.y, plyPos.x, plyPos.y) < 10000 then
-                    tooClose = true
-                    break
-                end
-            end
-
-            if not tooClose then
-                table.insert(possibleSpawns, ent)
-            end
-        end
-    end
-
-    return possibleSpawns ~= {}
 end
 
 Randomat:register(EVENT)
