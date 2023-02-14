f142f90e2260859d25fdce42773e0a2b3150bb0e
The Stig <TheStig294@users.noreply.github.com>
2023-02-14 16:13:08 +1030

    Updated whoa randomat to use new role changing method

--- lua/randomat2/events/whoa.lua
+++ lua/randomat2/events/whoa.lua
@@ -2,9 +2,9 @@ local modelExists = util.IsValidModel("models/bandicoot/bandicoot.mdl")
 util.PrecacheSound("whoa/whoa.mp3")
 local EVENT = {}
 
-CreateConVar("randomat_whoa_timer", 3, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Time between being given spin attacks")
+CreateConVar("randomat_whoa_timer", 3, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Time between being given spin attacks", 1, 15)
 
-CreateConVar("randomat_whoa_strip", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The event strips your other weapons")
+local strip = CreateConVar("randomat_whoa_strip", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "The event strips your other weapons")
 
 CreateConVar("randomat_whoa_weaponid", "weapon_ttt_whoa_randomat", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Id of the weapon given")
 
@@ -17,7 +17,7 @@ local function GetDescription()
 
     description = description .. " can"
 
-    if GetConVar("randomat_whoa_strip"):GetBool() then
+    if strip then
         description = description .. " only"
     end
 
@@ -29,35 +29,60 @@ end
 EVENT.Title = "Whoa!"
 EVENT.Description = GetDescription()
 EVENT.id = "whoa"
-EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
 
 EVENT.Categories = {"item", "largeimpact", "deathtrigger"}
 
-function EVENT:Begin()
-    self.Description = GetDescription()
+if strip then
+    EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
+    table.insert(EVENT.Categories, "rolechange")
+end
 
-    for i, ply in ipairs(self:GetAlivePlayers()) do
-        -- Convert all independent guys to innocents so we don't have to worry about fighting damage penalty logic
-        if Randomat:IsMonsterTeam(ply) or Randomat:IsIndependentTeam(ply) or Randomat:IsJesterTeam(ply) then
-            self:StripRoleWeapons(ply)
-            Randomat:SetRole(ply, ROLE_INNOCENT)
+function EVENT:HandleRoleWeapons(ply)
+    if not strip then return end
+    local updated = false
+    local changing_teams = Randomat:IsMonsterTeam(ply) or Randomat:IsIndependentTeam(ply)
+
+    -- Convert all bad guys to traitors so we don't have to worry about fighting with special weapon replacement logic
+    if (Randomat:IsTraitorTeam(ply) and ply:GetRole() ~= ROLE_TRAITOR) or changing_teams then
+        Randomat:SetRole(ply, ROLE_TRAITOR)
+        updated = true
+    elseif Randomat:IsJesterTeam(ply) then
+        Randomat:SetRole(ply, ROLE_INNOCENT)
+        updated = true
+    end
+
+    -- Remove role weapons from anyone on the traitor team now
+    if Randomat:IsTraitorTeam(ply) then
+        self:StripRoleWeapons(ply)
+    end
+
+    return updated, changing_teams
+end
+
+function EVENT:Begin()
+    strip = GetConVar("randomat_whoa_strip"):GetBool()
+    self.Description = GetDescription()
+    local new_traitors = {}
+
+    for _, v in ipairs(self:GetAlivePlayers()) do
+        local _, new_traitor = self:HandleRoleWeapons(v)
+
+        if new_traitor then
+            table.insert(new_traitors, v)
         end
     end
 
     SendFullStateUpdate()
+    self:NotifyTeamChange(new_traitors, ROLE_TEAM_TRAITOR)
 
-    for k, ply in pairs(player.GetAll()) do
-        if modelExists then
-            Randomat:ForceSetPlayermodel(ply, "models/bandicoot/bandicoot.mdl")
-        end
-    end
-
+    -- Periodically gives everyone the spin attack weapon
     timer.Create("RandomatWhoaTimer", GetConVar("randomat_whoa_timer"):GetInt(), 0, function()
         local weaponid = GetConVar("randomat_whoa_weaponid"):GetString()
+        local updated = false
 
-        for _, ply in pairs(self:GetAlivePlayers()) do
-            if GetConVar("randomat_whoa_strip"):GetBool() then
-                for _, wep in pairs(ply:GetWeapons()) do
+        for _, ply in ipairs(self:GetAlivePlayers()) do
+            if strip then
+                for _, wep in ipairs(ply:GetWeapons()) do
                     local weaponclass = WEPS.GetClass(wep)
 
                     if weaponclass ~= weaponid then
@@ -72,34 +97,69 @@ function EVENT:Begin()
             if not ply:HasWeapon(weaponid) then
                 ply:Give(weaponid)
             end
+
+            -- Workaround the case where people can respawn as Zombies while this is running
+            updatedPly, new_traitor = self:HandleRoleWeapons(ply)
+            updated = updated or updatedPly
+
+            if new_traitor then
+                table.insert(new_traitors, ply)
+            end
+        end
+
+        -- If anyone's role changed, update each client
+        -- If anyone became a traitor, notify all other traitors
+        if updated then
+            SendFullStateUpdate()
+            self:NotifyTeamChange(new_traitors, ROLE_TEAM_TRAITOR)
+            table.Empty(new_traitors)
         end
     end)
 
     self:AddHook("PlayerCanPickupWeapon", function(ply, wep)
-        if not GetConVar("randomat_whoa_strip"):GetBool() then return end
+        if not strip then return end
 
         return IsValid(wep) and WEPS.GetClass(wep) == GetConVar("randomat_whoa_weaponid"):GetString()
     end)
-
-    -- Sets someone's playermodel again when respawning, as force playermodel is off
-    if modelExists then
-        self:AddHook("PlayerSpawn", function(ply)
-            timer.Simple(1, function()
-                Randomat:ForceSetPlayermodel(ply, "models/bandicoot/bandicoot.mdl")
-            end)
-        end)
-    end
 
     self:AddHook("DoPlayerDeath", function(ply, attacker, dmginfo)
         -- Silence their usual death noise
         dmginfo:SetDamageType(DMG_SLASH)
         sound.Play("whoa/whoa.mp3", ply:GetShootPos(), 140, 100, 1)
     end)
+
+    -- Sets someone's playermodel again when respawning, as force playermodel is off
+    if modelExists then
+        for k, ply in pairs(player.GetAll()) do
+            Randomat:ForceSetPlayermodel(ply, "models/bandicoot/bandicoot.mdl")
+        end
+
+        self:AddHook("PlayerSpawn", function(ply)
+            timer.Simple(1, function()
+                Randomat:ForceSetPlayermodel(ply, "models/bandicoot/bandicoot.mdl")
+            end)
+        end)
+    end
 end
 
 function EVENT:End()
     timer.Remove("RandomatWhoaTimer")
-    Randomat:ForceResetAllPlayermodels()
+
+    for i, ent in ipairs(ents.FindByClass(GetConVar("randomat_whoa_weaponid"):GetString())) do
+        ent:Remove()
+    end
+
+    if strip then
+        for i, ply in ipairs(self:GetAlivePlayers()) do
+            ply:Give("weapon_zm_improvised")
+            ply:Give("weapon_zm_carry")
+            ply:Give("weapon_ttt_unarmed")
+        end
+    end
+
+    if modelExists then
+        Randomat:ForceResetAllPlayermodels()
+    end
 end
 
 function EVENT:Condition()
