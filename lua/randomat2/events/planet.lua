75ac381a855297f2d0a679aa76fcf8f78c3daf90
The Stig <TheStig294@users.noreply.github.com>
2023-04-10 11:51:56 +0930

    "My planet needs me!" now checks for body-dependent roles

--- lua/randomat2/events/planet.lua
+++ lua/randomat2/events/planet.lua
@@ -8,6 +8,23 @@ EVENT.Categories = {"fun", "deathtrigger", "moderateimpact"}
 local ragTimers = {}
 
 function EVENT:Begin()
+    local new_traitors = {}
+
+    for _, ply in ipairs(self:GetAlivePlayers()) do
+        if Randomat:IsBodyDependentRole(ply) then
+            self:StripRoleWeapons(ply)
+            local isTraitor = Randomat:SetToBasicRole(ply, "Traitor", true)
+
+            if isTraitor then
+                table.insert(new_traitors, ply)
+            end
+        end
+    end
+
+    -- Send message to the traitor team if new traitors joined
+    self:NotifyTeamChange(new_traitors, ROLE_TEAM_TRAITOR)
+    SendFullStateUpdate()
+
     self:AddHook("TTTOnCorpseCreated", function(rag)
         if not IsValid(rag) then return end
         local timerName = rag:EntIndex() .. "PlanetRandomat"
@@ -72,6 +89,20 @@ function EVENT:End()
     end
 
     table.Empty(ragTimers)
+end
+
+-- Checking if someone is a body dependent role and if it isn't at the start of the round, prevent the event from running
+function EVENT:Condition()
+    local bodyDependentRoleExists = false
+
+    for _, ply in ipairs(self:GetAlivePlayers()) do
+        if Randomat:IsBodyDependentRole(ply) then
+            bodyDependentRoleExists = true
+            break
+        end
+    end
+
+    return Randomat:GetRoundCompletePercent() < 5 or not bodyDependentRoleExists
 end
 
 Randomat:register(EVENT)
