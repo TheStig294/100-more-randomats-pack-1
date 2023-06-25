13526b432d66327178c8d062708ded49ef2761ad
The Stig <TheStig294@users.noreply.github.com>
2023-06-25 19:29:48 +0930

    "Unconventional healing" now ignores damage from map hazards and displays a message if killed by fire, explosion or fall damage from one, to prevent healing softlocks

--- lua/randomat2/events/unconventional.lua
+++ lua/randomat2/events/unconventional.lua
@@ -9,6 +9,16 @@ function EVENT:Begin()
     self:AddHook("EntityTakeDamage", function(ent, dmginfo)
         -- Don't affect negative damage sources, else this hook will lower a player's health
         if IsPlayer(ent) and dmginfo:GetDamage() > 0 and (dmginfo:IsDamageType(DMG_BURN) or dmginfo:IsDamageType(DMG_FALL) or dmginfo:IsDamageType(DMG_BLAST)) then
+            local attacker = dmginfo:GetAttacker()
+
+            -- Don't affect direct damage from map entities
+            if IsValid(attacker) and attacker:GetClass() == "trigger_hurt" then
+                ent:PrintMessage(HUD_PRINTCENTER, "You were killed by a map hazard!")
+                ent:PrintMessage(HUD_PRINTTALK, "You were killed by a map hazard!")
+
+                return
+            end
+
             -- Heal a player by the damage they would take, don't set them above max health
             ent:SetHealth(math.min(ent:Health() + dmginfo:GetDamage(), ent:GetMaxHealth()))
             -- Negate the damage they would take
